c065dd025f8a4fc2f47d6b9842f54362ad094af2
Trushkin, Ilya <ilya.trushkin@intel.com>
2021-03-04 18:06:21 +0300

    flake8 fix

--- openfl-workspace/torch_cnn_mnist/code/mnist_utils.py
+++ openfl-workspace/torch_cnn_mnist/code/mnist_utils.py
@@ -9,13 +9,13 @@ from logging import getLogger
 from torchvision.datasets import MNIST
 from torchvision.transforms import ToTensor
 
-logger = getLogger(__name__)
-
 # Remove this after upgrade to torchvision==0.9. See https://github.com/pytorch/vision/issues/3497
 from six.moves import urllib
 opener = urllib.request.build_opener()
 opener.addheaders = [('User-agent', 'Mozilla/5.0')]
 urllib.request.install_opener(opener)
+
+logger = getLogger(__name__)
 
 
 def one_hot(labels, classes):
