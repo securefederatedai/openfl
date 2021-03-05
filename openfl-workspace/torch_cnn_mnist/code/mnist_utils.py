dcd6e79af2906124a659b0df6b805b371256e35a
Ilya Trushkin <76161256+itrushkin@users.noreply.github.com>
2021-03-05 12:07:34 +0300

    Fix torchvision MNIST download (#26)
    * Fix torchvision MNIST download
    
    * flake8 fix
    
    * flake8 fix

--- openfl-workspace/torch_cnn_mnist/code/mnist_utils.py
+++ openfl-workspace/torch_cnn_mnist/code/mnist_utils.py
@@ -9,6 +9,12 @@ from logging import getLogger
 from torchvision.datasets import MNIST
 from torchvision.transforms import ToTensor
 
+# Remove this after upgrade to torchvision==0.9. See https://github.com/pytorch/vision/issues/3497
+from six.moves import urllib
+opener = urllib.request.build_opener()
+opener.addheaders = [('User-agent', 'Mozilla/5.0')]
+urllib.request.install_opener(opener)
+
 logger = getLogger(__name__)
 
 
