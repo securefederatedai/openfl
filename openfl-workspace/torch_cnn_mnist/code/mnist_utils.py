f31dec469a5862a6c86deca71ccbd3950e246f76
Trushkin, Ilya <ilya.trushkin@intel.com>
2021-03-12 12:35:39 +0300

    Fix 503 Service Unavailable error for MNIST

--- openfl-workspace/torch_cnn_mnist/code/mnist_utils.py
+++ openfl-workspace/torch_cnn_mnist/code/mnist_utils.py
@@ -6,14 +6,19 @@
 import numpy as np
 
 from logging import getLogger
-from torchvision.datasets import MNIST
-from torchvision.transforms import ToTensor
+from torchvision import datasets, transforms
 
-# Remove this after upgrade to torchvision==0.9. See https://github.com/pytorch/vision/issues/3497
-from six.moves import urllib
-opener = urllib.request.build_opener()
-opener.addheaders = [('User-agent', 'Mozilla/5.0')]
-urllib.request.install_opener(opener)
+# TODO: Remove after update to torchvision==0.9.1
+datasets.MNIST.resources = [
+    ('https://ossci-datasets.s3.amazonaws.com/mnist/train-images-idx3-ubyte.gz',
+        'f68b3c2dcbeaaa9fbdd348bbdeb94873'),
+    ('https://ossci-datasets.s3.amazonaws.com/mnist/train-labels-idx1-ubyte.gz',
+        'd53e105ee54ea40749a09fcbcd1e9432'),
+    ('https://ossci-datasets.s3.amazonaws.com/mnist/t10k-images-idx3-ubyte.gz',
+        '9fb629c4189551a2d022fa330f9573f3'),
+    ('https://ossci-datasets.s3.amazonaws.com/mnist/t10k-labels-idx1-ubyte.gz',
+        'ec29112dd5afa0611ce80d1b7f02629c')
+]
 
 logger = getLogger(__name__)
 
@@ -47,7 +52,7 @@ def _load_raw_datashards(shard_num, collaborator_count, transform=None):
         2 tuples: (image, label) of the training, validation dataset
     """
     train_data, val_data = (
-        MNIST('data', train=train, download=True, transform=transform)
+        datasets.MNIST('data', train=train, download=True, transform=transform)
         for train in (True, False)
     )
     X_train_tot, y_train_tot = train_data.train_data, train_data.train_labels
@@ -90,7 +95,7 @@ def load_mnist_shard(shard_num, collaborator_count,
     num_classes = 10
 
     (X_train, y_train), (X_valid, y_valid) = _load_raw_datashards(
-        shard_num, collaborator_count, transform=ToTensor())
+        shard_num, collaborator_count, transform=transforms.ToTensor())
 
     logger.info(f'MNIST > X_train Shape : {X_train.shape}')
     logger.info(f'MNIST > y_train Shape : {y_train.shape}')
