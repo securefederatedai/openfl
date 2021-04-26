Fixes to delta calculation and lossy compression flow. Added new workspace that enables lossy compression by default

openfl/pipelines/skc_pipeline.py
@@ -35,7 +35,6 @@ class SparsityTransformer(Transformer):
             condensed_data: an numpy array being sparsified.
             metadata: dictionary to store a list of meta information.
         """
-        self.p = 1
         metadata = {'int_list': list(data.shape)}
         # sparsification
         data = data.astype(np.float32)
@@ -115,12 +114,15 @@ class KmeansTransformer(Transformer):
         """
         # clustering
         data = data.reshape((-1, 1))
-        k_means = cluster.KMeans(
-            n_clusters=self.n_cluster, n_init=self.n_cluster)
-        k_means.fit(data)
-        quantized_values = k_means.cluster_centers_.squeeze()
-        indices = k_means.labels_
-        quant_array = np.choose(indices, quantized_values)
+        if data.shape[0] >= self.n_cluster:
+            k_means = cluster.KMeans(
+                n_clusters=self.n_cluster, n_init=self.n_cluster)
+            k_means.fit(data)
+            quantized_values = k_means.cluster_centers_.squeeze()
+            indices = k_means.labels_
+            quant_array = np.choose(indices, quantized_values)
+        else:
+            quant_array = data
         int_array, int2float_map = self._float_to_int(quant_array)
         metadata = {'int_to_float': int2float_map}
         int_array = int_array.reshape(-1)