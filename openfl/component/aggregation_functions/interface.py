e29ecba1cec1c63938d9eeb2f8e1481c97da608a
Ilya Trushkin <ilya.trushkin@intel.com>
2021-05-19 17:32:38 +0300

    Extend interface docstring

--- openfl/component/aggregation_functions/interface.py
+++ openfl/component/aggregation_functions/interface.py
@@ -24,11 +24,27 @@ class AggregationFunctionInterface(metaclass=SingletonABCMeta):
 
         Args:
             local_tensors(list[openfl.utilities.LocalTensor]): List of local tensors to aggregate.
-            db_iterator: iterator over history of all tensors.
-                Columns: ['tensor_name', 'round', 'tags', 'nparray']
+            db_iterator: iterator over history of all tensors. Columns:
+                - 'tensor_name': name of the tensor.
+                    Examples for `torch.nn.Module`s: 'conv1.weight', 'fc2.bias'.
+                - 'round': 0-based number of round corresponding to this tensor.
+                - 'tags': tuple of tensor tags. Tags that can appear:
+                    - 'model' indicates that the tensor is a model parameter.
+                    - 'trained' indicates that tensor is a part of a training result.
+                        These tensors are passed to the aggregator node after local learning.
+                    - 'aggregated' indicates that tensor is a result of aggregation.
+                        These tensors are sent to collaborators for the next round.
+                    - 'delta' indicates that value is a difference between rounds
+                        for a specific tensor.
+                    also one of the tags is a collaborator name
+                    if it corresponds to a result of a local task.
+
+                - 'nparray': value of the tensor.
             tensor_name: name of the tensor
             fl_round: round number
             tags: tuple of tags for this tensor
+        Returns:
+            np.ndarray: aggregated tensor
         """
         raise NotImplementedError
 
