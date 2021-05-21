3ed9f513adc09160ee04a63eb3e910b3b0fdfa65
Radionov, Maksim <maksim.radionov@intel.com>
2021-05-21 18:15:28 +0300

    Delete raising exceptions in addLoggingLevel function

--- openfl/utilities/utils.py
+++ openfl/utilities/utils.py
@@ -19,13 +19,6 @@ def addLoggingLevel(levelName, levelNum, methodName=None):
     if not methodName:
         methodName = levelName.lower()
 
-    if hasattr(logging, levelName):
-        raise AttributeError('{} already defined in logging module'.format(levelName))
-    if hasattr(logging, methodName):
-        raise AttributeError('{} already defined in logging module'.format(methodName))
-    if hasattr(logging.getLoggerClass(), methodName):
-        raise AttributeError('{} already defined in logger class'.format(methodName))
-
     def logForLevel(self, message, *args, **kwargs):
         if self.isEnabledFor(levelNum):
             self._log(levelNum, message, args, **kwargs)
