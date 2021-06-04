738f6cdc21b3dfbb4f246df8e729fe4c1a821ae8
amokrov <aleksandr.mokrov@intel.com>
2021-06-04 10:37:16 +0300

    Base shard descriptor

---
+++ openfl/services/shard_descriptor.py
@@ -0,0 +1,26 @@
+from openfl.services.shard_descriptor_base import ShardDescriptorBase
+
+
+class ShardDescriptor(ShardDescriptorBase):
+    def __init__(self, data_path) -> None:
+        super().__init__()
+        self.data_path = data_path
+        self.dataset_length = 100
+
+    def __len__(self):
+        return self.dataset_length
+
+    def get_item(self, index: int):
+        return None
+
+    @property
+    def sample_shape(self) -> int:
+        return 1
+
+    @property
+    def target_shape(self) -> int:
+        return 1
+
+    @property
+    def dataset_description(self) -> str:
+        return self.data_path
