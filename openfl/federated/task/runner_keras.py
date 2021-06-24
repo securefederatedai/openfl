74044e1bff142a60a7d95eff22dfb6d81c044ad7
Tony Reina <37851530+tonyreina@users.noreply.github.com>
2021-06-23 21:33:44 -0700

    Update runner_keras.py

--- openfl/federated/task/runner_keras.py
+++ openfl/federated/task/runner_keras.py
@@ -42,7 +42,7 @@ class KerasTaskRunner(TaskRunner):
         # functions in KerasTaskRunner
         self.required_tensorkeys_for_function = {}
         ke.backend.clear_session()
-        
+
     def rebuild_model(self, round_num, input_tensor_dict, validation=False):
         """
         Parse tensor names and update weights of model. Handles the optimizer treatment.
