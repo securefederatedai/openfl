f84029679b5968bd9b890b26429322e5731555f7
aleksandr-mokrov <76171391+aleksandr-mokrov@users.noreply.github.com>
2021-09-10 17:20:20 +0300

    Use only ShardDescriptor and Dataset for describe a shard data.

---
+++ openfl-tutorials/interactive_api/Director_Pytorch_TinyImageNet/envoy/base.py
@@ -0,0 +1,39 @@
+# Copyright (C) 2020-2021 Intel Corporation
+# SPDX-License-Identifier: Apache-2.0
+
+"""Base shard descriptor."""
+
+
+class Dataset:
+    """Dataset class."""
+
+    def __len__(self):
+        """Return the len of the dataset."""
+        raise NotImplementedError
+
+    def __getitem__(self, index):
+        """Return an item by the index."""
+        raise NotImplementedError
+
+
+class ShardDescriptor:
+    """Shard descriptor class."""
+
+    def get_dataset(self, dataset_type) -> Dataset:
+        """Return a dataset by type."""
+        raise NotImplementedError
+
+    @property
+    def sample_shape(self):
+        """Return the sample shape info."""
+        raise NotImplementedError
+
+    @property
+    def target_shape(self):
+        """Return the target shape info."""
+        raise NotImplementedError
+
+    @property
+    def dataset_description(self) -> str:
+        """Return the dataset description."""
+        return ''
