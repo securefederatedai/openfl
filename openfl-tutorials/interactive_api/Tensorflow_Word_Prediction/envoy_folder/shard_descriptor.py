21db207d94e334c17ed3a6ece0ccb037341d2d80
katerina-merkulova <ekaterina1.merkulova@intel.com>
2021-09-22 13:18:05 +0300

    fix comments

--- openfl-tutorials/interactive_api/Tensorflow_Word_Prediction/envoy_folder/shard_descriptor.py
+++ openfl-tutorials/interactive_api/Tensorflow_Word_Prediction/envoy_folder/shard_descriptor.py
@@ -1,6 +1,6 @@
 # Copyright (C) 2020-2021 Intel Corporation
 # SPDX-License-Identifier: Apache-2.0
-"""Market shard descriptor."""
+"""Shard descriptor for text."""
 
 import re
 import urllib.request
