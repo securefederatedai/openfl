10682c01c179ac6a60da67de2a1350dac36d629b
Khorkin, Alexey <alexey.khorkin@intel.com>
2021-10-13 13:31:29 +0300

    fix tests 4.0

--- tests/github/interactive_api_director/experiments/tensorflow_mnist/dataset.py
+++ tests/github/interactive_api_director/experiments/tensorflow_mnist/dataset.py
@@ -1,62 +1,90 @@
 import tensorflow as tf
 import numpy as np
 
+from tensorflow.keras.utils import Sequence
+
 from openfl.interface.interactive_api.experiment import DataInterface
 
 
+class DataGenerator(Sequence):
+
+    def __init__(self, shard_descriptor, batch_size):
+        self.shard_descriptor = shard_descriptor
+        self.batch_size = batch_size
+        self.indices = np.arange(len(shard_descriptor))
+        self.on_epoch_end()
+
+    def __len__(self):
+        return len(self.indices) // self.batch_size
+
+    def __getitem__(self, index):
+        index = self.indices[index * self.batch_size:(index + 1) * self.batch_size]
+        batch = [self.indices[k] for k in index]
+
+        X, y = self.shard_descriptor[batch]
+        return X, y
+
+    def on_epoch_end(self):
+        np.random.shuffle(self.indices)
+
+
 class FedDataset(DataInterface):
-    def __init__(self, train_bs, valid_bs, **kwargs):
+
+    def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.train_bs = train_bs
-        self.valid_bs = valid_bs
 
     @property
     def shard_descriptor(self):
         return self._shard_descriptor
-        
+
     @shard_descriptor.setter
     def shard_descriptor(self, shard_descriptor):
         """
         Describe per-collaborator procedures or sharding.
 
         This method will be called during a collaborator initialization.
-        Local shard_descriptor  will be set by Envoy.
+        Local shard_descriptor will be set by Envoy.
         """
         self._shard_descriptor = shard_descriptor
-        validation_size = len(self.shard_descriptor) // 10
-        self.train_indices = np.arange(len(self.shard_descriptor) - validation_size)
-        self.val_indices = np.arange(len(self.shard_descriptor) - validation_size, len(self.shard_descriptor))
 
-    def get_train_loader(self, **kwargs):
+    def __getitem__(self, index):
+        return self.shard_descriptor[index]
+
+    def __len__(self):
+        return len(self.shard_descriptor)
+
+    def get_train_loader(self):
         """
         Output of this method will be provided to tasks with optimizer in contract
         """
-        samples, targets = [], []
-        for i in self.train_indices:
-            sample, target = self.shard_descriptor[i]
-            samples.append(sample)
-            targets.append(target)
-        samples = np.array(samples)
-        targets = np.array(targets)
-        return tf.data.Dataset.from_tensor_slices((samples, targets)).batch(self.train_bs)
+        if self.kwargs['train_bs']:
+            batch_size = self.kwargs['train_bs']
+        else:
+            batch_size = 32
+        self.shard_descriptor.set_dataset_type(mode='train')
+        return DataGenerator(self.shard_descriptor, batch_size=batch_size)
 
-    def get_valid_loader(self, **kwargs):
+    def get_valid_loader(self):
         """
         Output of this method will be provided to tasks without optimizer in contract
         """
-        samples, targets = zip(*[self.shard_descriptor[i] for i in self.val_indices])
-        samples = np.array(samples)
-        targets = np.array(targets)
-        return tf.data.Dataset.from_tensor_slices((samples, targets)).batch(self.valid_bs)
+        if self.kwargs['valid_bs']:
+            batch_size = self.kwargs['valid_bs']
+        else:
+            batch_size = 32
+        
+        self.shard_descriptor.set_dataset_type(mode='val')
+        return DataGenerator(self.shard_descriptor, batch_size=batch_size)
 
     def get_train_data_size(self):
         """
         Information for aggregation
         """
-        return len(self.train_indices)
+        
+        return self.shard_descriptor.get_train_size()
 
     def get_valid_data_size(self):
         """
         Information for aggregation
         """
-        return len(self.val_indices)
+        return self.shard_descriptor.get_test_size()
