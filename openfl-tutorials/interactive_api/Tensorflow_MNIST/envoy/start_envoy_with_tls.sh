f217a5a1b3eca89b186674c2d018afbb31842333
igor-davidyuk <igor.davidyuk@intel.com>
2021-10-29 09:44:47 +0300

    more renamings: shard_config -> envoy_config

--- openfl-tutorials/interactive_api/Tensorflow_MNIST/envoy/start_envoy_with_tls.sh
+++ openfl-tutorials/interactive_api/Tensorflow_MNIST/envoy/start_envoy_with_tls.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 ENVOY_NAME=$1
-SHARD_CONF=$2
+ENVOY_CONF=$2
 DIRECTOR_FQDN=$3
 
-fx envoy start -n "$ENVOY_NAME" --envoy-config-path "$SHARD_CONF" -dh "$DIRECTOR_FQDN" -dp 50051 -rc cert/root_ca.crt -pk cert/"$ENVOY_NAME".key -oc cert/"$ENVOY_NAME".crt
+fx envoy start -n "$ENVOY_NAME" --envoy-config-path "$ENVOY_CONF" -dh "$DIRECTOR_FQDN" -dp 50051 -rc cert/root_ca.crt -pk cert/"$ENVOY_NAME".key -oc cert/"$ENVOY_NAME".crt
