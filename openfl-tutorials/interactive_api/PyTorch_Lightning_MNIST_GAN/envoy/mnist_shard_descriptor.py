1c91feff17f7fa08617a265c43a66f8fabc3c9f2
Mansi Sharma <mansi.sharma@intel.com>
2021-12-30 23:01:19 +0300

    adding GAN tutorial

---
+++ openfl-tutorials/interactive_api/PyTorch_Lightning_MNIST_GAN/envoy/mnist_shard_descriptor.py
@@ -0,0 +1,104 @@
+# Copyright (C) 2020-2021 Intel Corporation
+# SPDX-License-Identifier: Apache-2.0
+
+"""Mnist Shard Descriptor."""
+
+import logging
+import os
+from typing import List
+
+import numpy as np
+import requests
+from torchvision import datasets
+import torchvision.transforms as transforms
+
+from openfl.interface.interactive_api.shard_descriptor import ShardDataset
+from openfl.interface.interactive_api.shard_descriptor import ShardDescriptor
+
+logger = logging.getLogger(__name__)
+
+
+class MnistShardDataset(ShardDataset):
+    """Mnist Shard dataset class."""
+
+    def __init__(self, x, y, data_type, rank=1, worldsize=1):
+        """Initialize Mnist shard Dataset."""
+        self.data_type = data_type
+        self.rank = rank
+        self.worldsize = worldsize
+
+        self.x = x[self.rank - 1::self.worldsize]
+        self.y = y[self.rank - 1::self.worldsize]
+
+    def __getitem__(self, index: int):
+        """Return an item by the index."""
+        return self.x[index], self.y[index]
+
+    def __len__(self):
+        """Return the len of the dataset."""
+        return len(self.x)
+
+
+class MnistShardDescriptor(ShardDescriptor):
+    """Mnist Shard descriptor class."""
+
+    def __init__(
+            self,
+            rank_worldsize: str = '1, 1',
+            **kwargs
+    ):
+        """Initialize MnistShardDescriptor."""
+        self.rank, self.worldsize = tuple(int(num) for num in rank_worldsize.split(','))
+        (x_train, y_train), (x_val, y_val) = self.download_data()
+        self.data_by_type = {
+            'train': (x_train, y_train),
+            'val': (x_val, y_val)
+        }
+
+    def get_shard_dataset_types(self) -> List[str]:
+        """Get available shard dataset types."""
+        return list(self.data_by_type)
+
+    def get_dataset(self, dataset_type='train'):
+        """Return a shard dataset by type."""
+        if dataset_type not in self.data_by_type:
+            raise Exception(f'Wrong dataset type: {dataset_type}')
+        return MnistShardDataset(
+            *self.data_by_type[dataset_type],
+            data_type=dataset_type,
+            rank=self.rank,
+            worldsize=self.worldsize
+        )
+
+    @property
+    def sample_shape(self):
+        """Return the sample shape info."""
+        return ['28', '28']
+
+    @property
+    def target_shape(self):
+        """Return the target shape info."""
+        return ['1']
+
+    @property
+    def dataset_description(self) -> str:
+        """Return the dataset description."""
+        return (f'Mnist dataset, shard number {self.rank}'
+                f' out of {self.worldsize}')
+
+    def download_data(self):
+        """Download prepared dataset."""
+        transform = transforms.Compose(
+            [transforms.ToTensor(),
+            transforms.Normalize((0.1307,), (0.3081,)),])
+        train_data, val_data = (
+            datasets.MNIST('data', train=train, download=True, transform=transform)
+            for train in (True, False)
+        )
+        x_train, y_train = train_data.train_data, train_data.train_labels
+        x_val, y_val = val_data.test_data, val_data.test_labels
+        print("shape of x_train",x_train.shape)
+        print("shape of y_train", y_train.shape)
+
+        print('Mnist data was loaded!')
+        return (x_train, y_train), (x_val, y_val)
