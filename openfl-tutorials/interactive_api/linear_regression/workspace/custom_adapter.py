6f91e7f2b39af80296e8ba0bbd29084781ace26d
igor-davidyuk <iprotonv@protonmail.ch>
2022-01-20 15:08:02 +0300

    Alexandr's review

--- openfl-tutorials/interactive_api/linear_regression/workspace/custom_adapter.py
+++ openfl-tutorials/interactive_api/linear_regression/workspace/custom_adapter.py
@@ -10,10 +10,6 @@ from openfl.plugins.frameworks_adapters.framework_adapter_interface import (
 class CustomFrameworkAdapter(FrameworkAdapterPluginInterface):
     """Framework adapter plugin class."""
 
-    def __init__(self) -> None:
-        """Initialize framework adapter."""
-        pass
-
     @staticmethod
     def get_tensor_dict(model, optimizer=None):
         """Extract tensors from a model."""
