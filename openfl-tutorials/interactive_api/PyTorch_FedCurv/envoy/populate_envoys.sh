316be87d9e753bb646b12530e9641dd6d2d35c3c
Ilya Trushkin <ilya.trushkin@intel.com>
2022-01-24 11:27:50 +0300

    Fix envoy config structure

--- openfl-tutorials/interactive_api/PyTorch_FedCurv/envoy/populate_envoys.sh
+++ openfl-tutorials/interactive_api/PyTorch_FedCurv/envoy/populate_envoys.sh
@@ -3,12 +3,13 @@
 for i in {1..10}
 do
     mkdir $i
-    echo "template: tinyimagenet_shard_descriptor.TinyImageNetShardDescriptor
-params:
-    data_folder: tinyimagenet_data
-    rank_worldsize: $i,10
-" > $i/shard_config.yaml
-    echo "fx envoy start -n env_$i --disable-tls --shard-config-path shard_config.yaml -dh localhost -dp 50051" > $i/start_envoy.sh
+    echo "shard_descriptor:
+    template: tinyimagenet_shard_descriptor.TinyImageNetShardDescriptor
+    params:
+        data_folder: tinyimagenet_data
+        rank_worldsize: $i,10
+" > $i/envoy_config.yaml
+    echo "fx envoy start -n env_$i --disable-tls --envoy-config-path envoy_config.yaml -dh localhost -dp 50051" > $i/start_envoy.sh
     cp requirements.txt $i
     cp base.py $i
     cp tinyimagenet_shard_descriptor.py $i
