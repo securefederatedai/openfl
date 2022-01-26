23928c59f79e7661b36e3746b0510349277b11de
aleksandr-mokrov <76171391+aleksandr-mokrov@users.noreply.github.com>
2022-01-27 00:54:33 +0300

    compile_proto.sh was returned to protocols directory with correct paths. Basic proto readme was added.

--- compile_proto.sh
+++
@@ -1,8 +0,0 @@
-# Copyright (C) 2020-2021 Intel Corporation
-# SPDX-License-Identifier: Apache-2.0
-rm openfl/protocols/*_pb2.py openfl/protocols/*_pb2_grpc.py 2> /dev/null
-python -m pip install -qq grpcio-tools
-python -m grpc_tools.protoc -I . --python_out=. --grpc_python_out=. \
-  openfl/protocols/aggregator.proto \
-  openfl/protocols/director.proto
-python -m grpc_tools.protoc -I . --python_out=. openfl/protocols/base.proto
