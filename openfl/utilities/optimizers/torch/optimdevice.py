c7430cc1c9a9026f2c6ddd71ebefd31e57aa0bef
Mansi Sharma <mansi.sharma@intel.com>
2022-02-25 08:04:51 +0300

    fixed lint error

--- openfl/utilities/optimizers/torch/optimdevice.py
+++ openfl/utilities/optimizers/torch/optimdevice.py
@@ -4,14 +4,18 @@ import torch
 
 
 def optimizer_to(optim, device):
-    """This function is needed to transfer optimizer
-    parameters to the same device where the model is.
+    """Transfer optimizer to device.
+
+    Transfer optimizer's parameters to the same device where the model is.
     This is because optimized parameters must live in consistent locations
     when optimizers are constructed and used.
     Resembles model.to(device) for optimizer.
 
     Usage (in train task after the model is transferred to device):
             optimizer_to(optimizer,device)
+    Args:
+        optimizer   :  Torch optimizer
+        device      :  model's device
     """
     for param in optim.state.values():
         for subparam in param.values():
