4a8145ae945a0a1f10672657953b8d6168d9cea2
Ilya Trushkin <ilya.trushkin@intel.com>
2022-02-28 20:52:49 +0300

    Remove CUDA device specification

--- openfl-tutorials/interactive_api/PyTorch_Histology_FedCurv/envoy/start_envoys.sh
+++ openfl-tutorials/interactive_api/PyTorch_Histology_FedCurv/envoy/start_envoys.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -e
 
-cd 1 && CUDA_VISIBLE_DEVICES=0 bash start_envoy.sh &
-cd 2 && CUDA_VISIBLE_DEVICES=1 bash start_envoy.sh &
-cd 3 && CUDA_VISIBLE_DEVICES=2 bash start_envoy.sh &
-cd 4 && CUDA_VISIBLE_DEVICES=3 bash start_envoy.sh &
-cd 5 && CUDA_VISIBLE_DEVICES=0 bash start_envoy.sh &
-cd 6 && CUDA_VISIBLE_DEVICES=1 bash start_envoy.sh &
-cd 7 && CUDA_VISIBLE_DEVICES=2 bash start_envoy.sh &
-cd 8 && CUDA_VISIBLE_DEVICES=3 bash start_envoy.sh &
+cd 1 && bash start_envoy.sh &
+cd 2 && bash start_envoy.sh &
+cd 3 && bash start_envoy.sh &
+cd 4 && bash start_envoy.sh &
+cd 5 && bash start_envoy.sh &
+cd 6 && bash start_envoy.sh &
+cd 7 && bash start_envoy.sh &
+cd 8 && bash start_envoy.sh 
