bcb81f775492882a9a4baef8f8042ef0deef5cde
Saransh Agarwal <saransh.agarwal@gmail.com>
2022-03-22 11:57:05 +0100

    [linting flake8]

--- openfl-tutorials/interactive_api/PyTorch_BrainMRI_UNet/envoy/shard_descriptor.py
+++ openfl-tutorials/interactive_api/PyTorch_BrainMRI_UNet/envoy/shard_descriptor.py
@@ -3,7 +3,7 @@
 """BrainMRI Shard Descriptor"""
 
 import logging
-from typing import Any, Dict, List, Tuple
+from typing import Any, Tuple
 import os
 import numpy as np
 from openfl.interface.interactive_api.shard_descriptor import ShardDataset
@@ -11,8 +11,6 @@ from openfl.interface.interactive_api.shard_descriptor import ShardDescriptor
 import pandas as pd
 import torch
 import tarfile
-from torch.utils import data
-from torch.utils.data import Dataset, DataLoader
 import gdown
 import nibabel as nib
 from tqdm import tqdm
@@ -22,12 +20,13 @@ import json
 
 logger = logging.getLogger(__name__)
 
+
 class brainMRIShard(ShardDataset):
     """brainMRI Shard dataset class."""
 
     def __init__(self, csv_path, rank=1, worldsize=1,
-                crop_dims=None, dataset_dir='./',
-                augment=True, device='cpu', set='train') -> None:
+                 crop_dims=None, dataset_dir='./',
+                 augment=True, device='cpu', set='train') -> None:
         """Initialize brainMRIShardDataset."""
         self.rank = rank
         self.worldsize = worldsize
@@ -35,43 +34,40 @@ class brainMRIShard(ShardDataset):
         self.crop_dim = crop_dims
         self.dataset_dir = dataset_dir
         # Shard Dataset
-        if set=='train':
-            if self.rank==self.worldsize:
-                self.csv = self.csv.loc[(self.rank-1)*(
-                    len(self.csv)//self.worldsize):,:].reset_index(
+        if set == 'train':
+            if self.rank == self.worldsize:
+                self.csv = self.csv.loc[(self.rank - 1) * (
+                    len(self.csv) // self.worldsize):, :].reset_index(
                         drop=True)
             else:
-                self.csv = self.csv.loc[(self.rank-1)*(len(
-                    self.csv)//self.worldsize):(self.rank)*(len(
-                        self.csv)//self.worldsize),:].reset_index(drop=True)
+                self.csv = self.csv.loc[(self.rank - 1) * (len(
+                    self.csv) // self.worldsize):(self.rank) * (len(
+                        self.csv) // self.worldsize), :].reset_index(drop=True)
         self.device = device
         self.augment = augment
 
-
     def __getitem__(self, idx) -> Tuple[Any, Any]:
         """Return a item by the index."""
-        img_path = os.path.join(self.dataset_dir,self.csv.loc[idx,'img_path'])
-        msk_path = os.path.join(self.dataset_dir,self.csv.loc[idx,'msk_path'])
+        img_path = os.path.join(self.dataset_dir, self.csv.loc[idx, 'img_path'])
+        msk_path = os.path.join(self.dataset_dir, self.csv.loc[idx, 'msk_path'])
         img = np.load(img_path)
         msk = np.load(msk_path)
         if self.crop_dim is None:
-            self.crop_dim = [img.shape[0],img.shape[1]]
+            self.crop_dim = [img.shape[0], img.shape[1]]
         img = self.preprocess_img(img)
         msk = self.preprocess_label(msk)
-        img,msk = self.crop_input(img,msk)
+        img, msk = self.crop_input(img, msk)
         if self.augment:
-            img,msk = self.augment_data(img,msk)
-        img = img[np.newaxis,:,:].copy()
+            img, msk = self.augment_data(img, msk)
+        img = img[np.newaxis, :, :].copy()
         msk = msk.copy()
         img = torch.tensor(img).to(self.device).float()
         msk = torch.tensor(msk).to(self.device).int()
-        return img,msk
-
+        return img, msk
 
     def __len__(self) -> int:
         """Return the len of the dataset."""
         return len(self.csv)
-
 
     def crop_input(self, img, msk) -> Tuple[Any, Any]:
         """
@@ -81,7 +77,7 @@ class brainMRIShard(ShardDataset):
         if self.crop_dim[0] == -1:
             self.crop_dim[0] = img.shape[0]
         if self.crop_dim[1] == -1:
-            self.crop_dim[1] = img.shape[1]  
+            self.crop_dim[1] = img.shape[1]
 
         slices = []
 
@@ -93,47 +89,45 @@ class brainMRIShard(ShardDataset):
             cropLen = self.crop_dim[idx]
             imgLen = img.shape[idy]
 
-            start = (imgLen-cropLen)//2
+            start = (imgLen - cropLen) // 2
 
             ratio_crop = 0.20  # Crop up this this % of pixels for offset
             # Number of pixels to offset crop in this dimension
-            offset = int(np.floor(start*ratio_crop))
+            offset = int(np.floor(start * ratio_crop))
 
             if offset > 0:
                 if is_random:
                     start += np.random.choice(range(-offset, offset))
                     if ((start + cropLen) > imgLen):  # Don't fall off the image
-                        start = (imgLen-cropLen)//2
+                        start = (imgLen - cropLen) // 2
             else:
                 start = 0
 
-            slices.append(slice(start, start+cropLen))
+            slices.append(slice(start, start + cropLen))
 
         return img[tuple(slices)], msk[tuple(slices)]
 
     def augment_data(self, img, msk) -> Tuple[Any, Any]:
         if np.random.rand() > 0.5:
-            ax = np.random.choice([0,1])
+            ax = np.random.choice([0, 1])
             img = np.flip(img, ax)
             msk = np.flip(msk, ax)
 
         if np.random.rand() > 0.5:
             rot = np.random.choice([1, 2, 3])  # 90, 180, or 270 degrees
 
-            img = np.rot90(img, rot, axes=[0,1])  # Rotate axes 0 and 1
-            msk = np.rot90(msk, rot, axes=[0,1])  # Rotate axes 0 and 1
+            img = np.rot90(img, rot, axes=[0, 1])  # Rotate axes 0 and 1
+            msk = np.rot90(msk, rot, axes=[0, 1])  # Rotate axes 0 and 1
 
         return img, msk
 
     def preprocess_img(self, img) -> Any:
         EPSILON = 1e-8
         return (img - img.mean()) / (img.std() + EPSILON)
-    
 
     def preprocess_label(self, label) -> Any:
-        label[label> 0] = 1.0
+        label[label > 0] = 1.0
         return label
-
 
 
 class brainMRIShardDescriptor(ShardDescriptor):
@@ -165,17 +159,16 @@ class brainMRIShardDescriptor(ShardDescriptor):
         self._sample_shape = [str(dim) for dim in sample.shape]
         self._target_shape = [str(dim) for dim in target.shape]
 
-
     def get_dataset(self, dataset_type='train') -> brainMRIShard:
         # """Return a shard dataset by type."""
-        if dataset_type=='train':
+        if dataset_type == 'train':
             return brainMRIShard(
                 csv_path=self.train_csv,
                 rank=self.rank,
                 worldsize=self.worldsize,
                 crop_dims=self.crop_dims
             )
-        elif dataset_type=='valid':
+        elif dataset_type == 'valid':
             return brainMRIShard(
                 csv_path=self.valid_csv,
                 rank=-1,
@@ -183,8 +176,8 @@ class brainMRIShardDescriptor(ShardDescriptor):
                 crop_dims=self.crop_dims,
                 augment=False,
                 set='valid'
-            )    
-    
+            )
+
     @property
     def sample_shape(self):
         """Return the sample shape info."""
@@ -200,7 +193,7 @@ class brainMRIShardDescriptor(ShardDescriptor):
         """Return the dataset description."""
         return (f'brainMRI dataset, shard number {self.rank} '
                 f'out of {self.worldsize}')
-    
+
     @staticmethod
     def download_dataset() -> None:
         """Helper function to download the dataset"""
@@ -208,20 +201,20 @@ class brainMRIShardDescriptor(ShardDescriptor):
         if os.path.exists('Task01_BrainTumour.tar'):
             return None
         url = 'https://drive.google.com/uc?id=1A2IU8Sgea1h3fYLpYtFb2v7NYdMjvEhU'
-        gdown.download(url,output='Task01_BrainTumour.tar',quiet=False)
+        gdown.download(url, output='Task01_BrainTumour.tar', quiet=False)
         # use tarfile to extract the data
         tar = tarfile.open('./Task01_BrainTumour.tar')
         tar.extractall()
         tar.close()
-        # Now read the data and process it to make the necessary images in 
+        # Now read the data and process it to make the necessary images in
         # the way we want it to be
         data_folder = './Task01_BrainTumour/'
         dataset_json_path = data_folder + 'dataset.json'
-        with open(dataset_json_path,'r') as f:
+        with open(dataset_json_path, 'r') as f:
             dataset_json = json.load(f)
         image_list = dataset_json['training']
         num_images = len(image_list)
-        num_train = int(np.floor(num_images*.80))
+        num_train = int(np.floor(num_images * .80))
         train_image_list = image_list[:num_train]
         valid_image_list = image_list[num_train:]
         # The processed images are saved in ./processed_data/ folder
@@ -230,81 +223,78 @@ class brainMRIShardDescriptor(ShardDescriptor):
             print("processed_data/ already exists, do you want to proceed? \
                 press y/Y to proceed and n/N to cancel")
             inp = input()
-            if inp=='y' or inp=='Y':
+            if inp == 'y' or inp == 'Y':
                 pass
             else:
                 sys.exit("Exiting program")
         else:
             os.mkdir(save_folder)
 
-
         img_path_new = save_folder + 'img/'
         if os.path.exists(img_path_new):
             print("processed_data/img/ already exists, \
                 do you want to proceed? press y/Y to proceed and n/N to cancel")
             inp = input()
-            if inp=='y' or inp=='Y':
+            if inp == 'y' or inp == 'Y':
                 pass
             else:
                 sys.exit("Exiting program")
         else:
             os.mkdir(img_path_new)
 
-
         msk_path_new = save_folder + 'msk/'
         if os.path.exists(msk_path_new):
             print("processed_data/msk/ already exists, do you \
                 want to proceed? press y/Y to proceed and n/N to cancel")
             inp = input()
-            if inp=='y' or inp=='Y':
+            if inp == 'y' or inp == 'Y':
                 pass
             else:
                 sys.exit("Exiting program")
         else:
             os.mkdir(msk_path_new)
 
-
         # Two CSV files 'train_data.csv' and 'valid_data.csv' are created which
         #  will have the maps
         train_data_csv = './train_data.csv'
         valid_data_csv = './valid_data.csv'
-        with open(train_data_csv,'w') as f:
+        with open(train_data_csv, 'w') as f:
             f.write('name,img_path,msk_path\n')
-        with open(valid_data_csv,'w') as f:
+        with open(valid_data_csv, 'w') as f:
             f.write('name,img_path,msk_path\n')
 
         # process the train images
         for t in tqdm(train_image_list):
-            img_name = t['image'][t['image'].rfind('/')+1:t['image'].rfind('.nii')]
+            img_name = t['image'][t['image'].rfind('/') + 1:t['image'].rfind('.nii')]
             img_path = os.path.join(data_folder, t['image'])
             msk_path = os.path.join(data_folder, t['label'])
-            img = np.array(nib.load(img_path).dataobj)[:,:,:,0]
+            img = np.array(nib.load(img_path).dataobj)[:, :, :, 0]
             msk = np.array(nib.load(msk_path).dataobj)
             num_slices = msk.shape[2]
             for slice in range(num_slices):
-                img_slice = img[:,:,slice]
-                msk_slice = msk[:,:,slice]
-                img_slice_name = os.path.join(img_path_new,f'{img_name}_{slice}.npy')
-                msk_slice_name = os.path.join(msk_path_new,f'{img_name}_{slice}.npy')
-                np.save(img_slice_name,img_slice)
-                np.save(msk_slice_name,msk_slice)
-                with open(train_data_csv,'a') as f:
+                img_slice = img[:, :, slice]
+                msk_slice = msk[:, :, slice]
+                img_slice_name = os.path.join(img_path_new, f'{img_name}_{slice}.npy')
+                msk_slice_name = os.path.join(msk_path_new, f'{img_name}_{slice}.npy')
+                np.save(img_slice_name, img_slice)
+                np.save(msk_slice_name, msk_slice)
+                with open(train_data_csv, 'a') as f:
                     f.write(f'{img_name},{img_slice_name},{msk_slice_name}\n')
 
         # process the validation images
         for v in tqdm(valid_image_list):
-            img_name = v['image'][v['image'].rfind('/')+1:v['image'].rfind('.nii')]
+            img_name = v['image'][v['image'].rfind('/') + 1:v['image'].rfind('.nii')]
             img_path = os.path.join(data_folder, v['image'])
             msk_path = os.path.join(data_folder, v['label'])
-            img = np.array(nib.load(img_path).dataobj)[:,:,:,0]
+            img = np.array(nib.load(img_path).dataobj)[:, :, :, 0]
             msk = np.array(nib.load(msk_path).dataobj)
             num_slices = msk.shape[2]
             for slice in range(num_slices):
-                img_slice = img[:,:,slice]
-                msk_slice = msk[:,:,slice]
-                img_slice_name = os.path.join(img_path_new,f'{img_name}_{slice}.npy')
-                msk_slice_name = os.path.join(msk_path_new,f'{img_name}_{slice}.npy')
-                np.save(img_slice_name,img_slice)
-                np.save(msk_slice_name,msk_slice)
-                with open(valid_data_csv,'a') as f:
+                img_slice = img[:, :, slice]
+                msk_slice = msk[:, :, slice]
+                img_slice_name = os.path.join(img_path_new, f'{img_name}_{slice}.npy')
+                msk_slice_name = os.path.join(msk_path_new, f'{img_name}_{slice}.npy')
+                np.save(img_slice_name, img_slice)
+                np.save(msk_slice_name, msk_slice)
+                with open(valid_data_csv, 'a') as f:
                     f.write(f'{img_name},{img_slice_name},{msk_slice_name}\n')
