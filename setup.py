aadbb562e03964a4451a4163fbb81da3fdf02035
Mansi Sharma <mansi.sharma@intel.com>
2022-06-01 10:06:06 -0700

    fix cli for pb version

--- setup.py
+++ setup.py
@@ -142,7 +142,7 @@ setup(
         'jupyterlab',
         'numpy',
         'pandas',
-        'protobuf',
+        'protobuf<=3.20.1',
         'requests',
         'rich',
         'scikit-learn',
