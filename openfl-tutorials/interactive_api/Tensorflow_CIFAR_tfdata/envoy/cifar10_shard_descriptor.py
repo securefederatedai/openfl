ca468dd5b0fe7627392aadeddbb148a39e69103b
Shah, Karan <karan.shah@intel.com>
2022-06-02 19:14:07 +0530

    Bump copyright header
    Signed-off-by: Shah, Karan <karan.shah@intel.com>

--- openfl-tutorials/interactive_api/Tensorflow_CIFAR_tfdata/envoy/cifar10_shard_descriptor.py
+++ openfl-tutorials/interactive_api/Tensorflow_CIFAR_tfdata/envoy/cifar10_shard_descriptor.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2020-2021 Intel Corporation
+# Copyright (C) 2022 Intel Corporation
 # SPDX-License-Identifier: Apache-2.0
 
 """CIFAR10 Shard Descriptor (using `tf.data.Dataset` API)"""
