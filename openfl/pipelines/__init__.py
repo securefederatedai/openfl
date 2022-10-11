b6de67bf2c9826bb4797830fb898e72a3e111db4
Yaniv Ben-Itzhak <yaniv.benizhak@gmail.com>
2022-10-11 09:12:32 +0300

    conditional import for EDEN pipeline
    Co-Authored-By: Shay Vargaftik <16802840+shayvar@users.noreply.github.com>

--- openfl/pipelines/__init__.py
+++ openfl/pipelines/__init__.py
@@ -1,9 +1,14 @@
 # Copyright (C) 2020-2021 Intel Corporation
+# Copyright 2022 VMware, Inc.
 # SPDX-License-Identifier: Apache-2.0
 """openfl.pipelines module."""
 
+import pkgutil
+
+if pkgutil.find_loader('torch'):
+    from .eden_pipeline import EdenPipeline  # NOQA
+
 from .kc_pipeline import KCPipeline
-from .eden_pipeline import EdenPipeline
 from .no_compression_pipeline import NoCompressionPipeline
 from .random_shift_pipeline import RandomShiftPipeline
 from .skc_pipeline import SKCPipeline
