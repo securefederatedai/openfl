a3c526acef160380a5698e7643aca509cf5823d8
igor-davidyuk <igor.davidyuk@intel.com>
2022-11-02 14:37:12 +0200

    method functioning

--- openfl/interface/model.py
+++ openfl/interface/model.py
@@ -1,14 +1,19 @@
 # Copyright (C) 2020-2022 Intel Corporation
 # SPDX-License-Identifier: Apache-2.0
-"""Plan module."""
+"""Model CLI module."""
 
 from logging import getLogger
+from pathlib import Path
+from typing import Union
 
-from click import echo
 from click import group
 from click import option
 from click import pass_context
 from click import Path as ClickPath
+from openfl.federated import Plan
+from openfl.federated import TaskRunner
+from openfl.protocols import utils
+from openfl.pipelines import NoCompressionPipeline
 
 logger = getLogger(__name__)
 
@@ -16,59 +21,68 @@ logger = getLogger(__name__)
 @group()
 @pass_context
 def model(context):
-    """Manage Federated Learning Plans."""
+    """Manage Federated Learning Models."""
     context.obj['group'] = 'model'
 
 
-@model.command()
+@model.command(name='save')
 @pass_context
-@option('-p', '--plan_config', required=False,
+@option('-p', '--plan-config', required=False,
         help='Federated learning plan [plan/plan.yaml]',
         default='plan/plan.yaml', type=ClickPath(exists=True))
-@option('-c', '--cols_config', required=False,
+@option('-c', '--cols-config', required=False,
         help='Authorized collaborator list [plan/cols.yaml]',
         default='plan/cols.yaml', type=ClickPath(exists=True))
-@option('-d', '--data_config', required=False,
+@option('-d', '--data-config', required=False,
         help='The data set/shard configuration file [plan/data.yaml]',
         default='plan/data.yaml', type=ClickPath(exists=True))
-@option('-i', '--input', 'input_protobuf', required=True,
-        help='The model protobuf to convert')
-@option('-o', '--output', required=False,
+@option('-i', '--input', 'model_protobuf_path', required=True,
+        help='The model protobuf to convert',
+        type=ClickPath(exists=True))
+@option('-o', '--output', 'output_filepath', required=False,
         help='Filename the model will be saved to in native format',
-        default='output_model')
-def save(context, plan_config, cols_config, data_config, input_protobuf, output):
+        default='output_model', type=ClickPath())
+def save_(context, plan_config, cols_config, data_config, model_protobuf_path, output_filepath):
     """
-    Save the model in native format (PyTorch / Tensorflow).
+    Save the model in native format (PyTorch / Keras).
     """
-    from pathlib import Path
+    save_model(plan_config, cols_config, data_config, model_protobuf_path, output_filepath)
 
-    from openfl.federated import Plan
-    from openfl.protocols import utils
-    from openfl.utilities import split_tensor_dict_for_holdouts
-    from openfl.utilities.utils import getfqdn_env
-    from openfl.pipelines import NoCompressionPipeline
+
+def save_model(
+    plan_config: str,
+    cols_config: str,
+    data_config: str,
+    model_protobuf_path: str,
+    output_filepath: str = None
+) -> Union[None, TaskRunner]:
+    """
+    Save the model in native format (PyTorch / Keras).
+    """
+    model_protobuf_path = Path(model_protobuf_path).absolute()
+    output_filepath = Path(output_filepath).absolute()
 
     plan = Plan.parse(plan_config_path=Path(plan_config),
                       cols_config_path=Path(cols_config),
                       data_config_path=Path(data_config))
 
-    init_state_path = plan.config['aggregator']['settings']['init_state_path']
+    collaborator_name = list(plan.cols_data_paths)[0]
+    data_loader = plan.get_data_loader(collaborator_name)
+    task_runner = plan.get_task_runner(data_loader=data_loader)
 
-    collaborator_cname = list(plan.cols_data_paths)[0]
+    logger.info(f'Loading OpenFL model protobuf:  🠆 {model_protobuf_path}')
 
-    data_loader = plan.get_data_loader(collaborator_cname)
-    task_runner = plan.get_task_runner(data_loader)
-    tensor_pipe = plan.get_tensor_pipe()
+    model_protobuf = utils.load_proto(model_protobuf_path)
 
-    logger.info(f'Loading OpenFL model protobuf:  🠆 {input_protobuf}')
+    tensor_dict, _ = utils.deconstruct_model_proto(model_protobuf, NoCompressionPipeline())
 
-    model = utils.load_proto(input_protobuf)
+    # This may break for multiple models.
+    # task_runner.set_tensor_dict will need to handle multiple models
+    task_runner.set_tensor_dict(tensor_dict, with_opt_vars=False)
 
-    tensor_dict, round_number = utils.deconstruct_model_proto(model, NoCompressionPipeline())
-
-    # This may break for multiple models. task_runner.set_tensor_dict will need to handle multiple models
-    task_runner.set_tensor_dict(tensor_dict)
-
-    task_runner.save_native(output)
-
-    logger.info(f'Saved model in native format:  🠆 {output}')
+    if output_filepath:
+        task_runner.save_native(output_filepath)
+        logger.info(f'Saved model in native format:  🠆 {output_filepath}')
+    else:
+        del task_runner.data_loader
+        return task_runner
