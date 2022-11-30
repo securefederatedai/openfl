c35f799d94b3a9a533fee6adc1a212afd285b39f
Patrick Foley <psfoley@gmail.com>
2022-11-29 16:58:09 -0800

    Update grpc / protobuf versions. Python 3.6 not supported by latest grpc versions
    Signed-off-by: Patrick Foley <psfoley@gmail.com>

--- setup.py
+++ setup.py
@@ -143,7 +143,7 @@ setup(
         'jupyterlab',
         'numpy',
         'pandas',
-        'protobuf==3.19.5',
+        'protobuf==4.21.9',
         'requests',
         'rich',
         'scikit-learn',
@@ -151,8 +151,8 @@ setup(
         'tensorboardX',
         'tqdm',
     ],
-    setup_requires=['grpcio-tools~=1.48.2'],
-    python_requires='>=3.6, <3.11',
+    setup_requires=['grpcio-tools~=1.51.1'],
+    python_requires='>=3.7, <3.11',
     project_urls={
         'Bug Tracker': 'https://github.com/intel/openfl/issues',
         'Documentation': 'https://openfl.readthedocs.io/en/stable/',
@@ -173,7 +173,6 @@ setup(
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
