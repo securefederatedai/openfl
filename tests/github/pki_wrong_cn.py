bf8ef6963253770cdf7937d7b869fe995f258c90
Ilya Trushkin <ilya.trushkin@intel.com>
2022-12-07 18:24:31 +0200

    Test cert checking
    Signed-off-by: Ilya Trushkin <ilya.trushkin@intel.com>

--- tests/github/pki_wrong_cn.py
+++ tests/github/pki_wrong_cn.py
@@ -1,35 +1,80 @@
 # Copyright (C) 2020-2021 Intel Corporation
 # SPDX-License-Identifier: Apache-2.0
-import socket
+import shutil
 import subprocess
 import os
+import time
+import socket
+from subprocess import Popen
+import sys
+
+from openfl.native import setup_plan
 
 
-def test_aggregator_common_name(common_name, should_fail):
-    exit_code = subprocess.call([
-        'fx', 'aggregator', 'generate-cert-request',
-        '--fqdn', common_name
+def prepare_workspace():
+    subprocess.check_call(['fx', 'workspace', 'certify'])
+    subprocess.check_call(['fx', 'plan', 'initialize'])
+
+    subprocess.check_call([
+        'fx', 'aggregator', 'generate-cert-request'
     ])
-    if should_fail != bool(exit_code):
-        raise Exception(f'Aggregator CSR generation command finished with exit code {exit_code}. '
-                        f'Common Name: "{common_name}".')
+    subprocess.check_call([
+        'fx', 'aggregator', 'certify',
+        '-s'
+    ])
+    for col in ['one', 'two']:
+        subprocess.check_call([
+            'fx', 'collaborator', 'generate-cert-request',
+            '-n', col,
+            '-d', '1',
+            '-s', '-x'
+        ])
+        subprocess.check_call([
+            'fx', 'collaborator', 'certify',
+            '-n', col,
+            '-s'
+        ])
+    
+    sys.path.append(os.getcwd())
+    
+def start_invalid_collaborator():
+    '''
+    We choose the gRPC client of another collaborator
+    to check if aggregator would accept certificate
+    that does not correspond to the collaborator's name.
+    '''
+    col_name = 'one'
+    plan = setup_plan()
+    plan.resolve()
+    client = plan.get_client(
+        'two', 
+        plan.aggregator_uuid,
+        plan.federation_uuid
+        )
+    collaborator = plan.get_collaborator(col_name, client=client)
+    collaborator.run()
 
 
 if __name__ == '__main__':
+    origin_dir = os.getcwd()
     prefix = 'fed_workspace'
     subprocess.check_call([
         'fx', 'workspace', 'create',
-        '--prefix', str(prefix),
+        '--prefix', prefix,
         '--template', 'keras_cnn_mnist'
     ])
     os.chdir(prefix)
-    subprocess.check_call(['fx', 'workspace', 'certify'])
-
-    # invalid common name
-    test_aggregator_common_name('not_an_fqdn', should_fail=True)
-
-    # case when CA is located on separate machine
-    test_aggregator_common_name('some.fqdn.com', should_fail=False)
-
-    # valid common name
-    test_aggregator_common_name(socket.getfqdn(), should_fail=False)
+    fqdn = socket.getfqdn()
+    try:
+        prepare_workspace()
+        agg = Popen(target=subprocess.check_call, args=[['fx','aggregator', 'start']])
+        agg.start()
+        time.sleep(3)
+        print('Starting Collaborator...')
+        start_invalid_collaborator()
+        agg.join()
+        assert False, 'Aggregator accepted invalid collaborator certificate.'
+    finally:
+        agg.kill()
+        os.chdir(origin_dir)
+        shutil.rmtree(prefix)
