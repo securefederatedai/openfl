2c07aa9ade29dd5aa758a83fa9b9d637443a1996
Ilya Trushkin <ilya.trushkin@intel.com>
2022-12-09 23:17:18 +0200

    use shell
    Signed-off-by: Ilya Trushkin <ilya.trushkin@intel.com>

--- tests/github/interactive_api_director/experiments/pytorch_kvasir_unet/run.py
+++ tests/github/interactive_api_director/experiments/pytorch_kvasir_unet/run.py
@@ -2,38 +2,43 @@ import time
 import sys
 from subprocess import Popen, check_call
 import psutil
-import importlib
 from pathlib import Path
 
-if __name__ == '__main__':
+
+def start_director():
     director_dir = Path(__file__).parent / 'director'
-    bg_procs = []
-    proc = Popen(
-        ['fx', 'director', 'start', '--disable-tls', '-c', 'director_config.yaml'],
-        cwd=director_dir, shell=False)
-    bg_procs.append(proc)
+    director = Popen('fx director start '
+                     '--disable-tls '
+                     '-c director_config.yaml',
+                     cwd=director_dir, shell=True)
     time.sleep(3)
-    if proc.pid not in psutil.pids():
+    if director.pid not in psutil.pids():
         print('Error: failed to create director')
         sys.exit(1)
+    return director
 
+
+def start_envoy():
     envoy_dir = Path(__file__).parent / 'envoy'
     check_call(
         [sys.executable, '-m', 'pip', 'install', '-r', 'sd_requirements.txt'], cwd=envoy_dir
     )
-    proc = Popen(
-        ['fx', 'envoy', 'start', '-n', 'env_one', '--disable-tls', '--envoy-config-path',
-            'envoy_config.yaml', '-dh', 'localhost', '-dp', '50051'], cwd=envoy_dir, shell=False
-    )
-
-    bg_procs.append(proc)
+    envoy = Popen('fx envoy start '
+                  '-n env_one '
+                  '--disable-tls '
+                  '--envoy-config-path envoy_config.yaml '
+                  '-dh localhost '
+                  '-dp 50051',
+                  cwd=envoy_dir, shell=True)
     time.sleep(10)
-    if proc.pid not in psutil.pids():
+    if envoy.pid not in psutil.pids():
         print('Error: failed to create envoy')
         sys.exit(1)
-    experiment = importlib.import_module(
-        'tests.github.interactive_api_director.experiments.pytorch_kvasir_unet.experiment'
-    )
+    return envoy
+
+
+if __name__ == '__main__':
+    director = start_director()
+    envoy = start_envoy()
+    from tests.github.interactive_api_director.experiments.pytorch_kvasir_unet import experiment
     experiment.run()
-    for proc in bg_procs:
-        proc.kill()
