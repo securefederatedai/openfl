b1792ac3c40376f93d3dbdd0f9ad118956559fd5
Sachin Gupta <sachin.gupta.dsp@gmail.com>
2022-12-13 12:11:23 +0530

    Fix lint warnings in openfl/experimental/runtime/runtime.py - ./openfl/experimental/runtime/runtime.py:24:100: E501 line too long (102 > 99 characters) - ./openfl/experimental/runtime/runtime.py:35:26: E711 comparison to None should be 'if cond is not None:' - ./openfl/experimental/runtime/runtime.py:60:16: F541 f-string is missing placeholders

--- openfl/experimental/runtime/runtime.py
+++ openfl/experimental/runtime/runtime.py
@@ -16,12 +16,19 @@ class Runtime:
 
 
 class LocalRuntime(Runtime):
-    def __init__(self, aggregator=None, collaborators=None, backend="single_process", **kwargs):
+    def __init__(
+        self,
+        aggregator=None,
+        collaborators=None,
+        backend="single_process",
+        **kwargs,
+    ):
         """Use remote federated infrastructure to run the flow"""
         super().__init__()
         if backend not in ["ray", "single_process"]:
             raise ValueError(
-                f"Invalid 'backend' value '{backend}', accepted values are 'ray', or 'single_process'"
+                f"Invalid 'backend' value '{backend}', accepted values are 'ray', or "
+                + "'single_process'"
             )
         if backend == "ray":
             if not ray.is_initialized():
@@ -32,7 +39,7 @@ class LocalRuntime(Runtime):
         self.aggregator = aggregator
         # List of envoys should be iterable, so that a subset can be selected at runtime
         # The envoys is the superset of envoys that can be selected during the experiment
-        if collaborators != None:
+        if collaborators is not None:
             self.collaborators = collaborators
 
     @property
@@ -57,10 +64,12 @@ class LocalRuntime(Runtime):
         }
 
     def __repr__(self):
-        return f"LocalRuntime"
+        return "LocalRuntime"
 
 
 class FederatedRuntime(Runtime):
     def __init__(self, aggregator, collaborators=None):
         """Use remote federated infrastructure to run the flow"""
-        raise NotImplementedError("FederatedRuntime will be implemented in the future")
+        raise NotImplementedError(
+            "FederatedRuntime will be implemented in the future"
+        )
