20d0685734a9db077545e125ddbac79c5638979e
Ilya Trushkin <ilya.trushkin@intel.com>
2022-12-14 22:48:35 +0200

    Move tensorflow interactive API test to Python
    Signed-off-by: Ilya Trushkin <ilya.trushkin@intel.com>

--- tests/github/interactive_api_director/experiments/tensorflow_mnist/run.py
+++ tests/github/interactive_api_director/experiments/tensorflow_mnist/run.py
@@ -6,5 +6,5 @@ from tests.github.interactive_api_director import utils
 if __name__ == '__main__':
     director = utils.start_director(Path(__file__).parent / 'director')
     envoy = utils.start_envoy(Path(__file__).parent / 'envoy')
-    from tests.github.interactive_api_director.experiments.pytorch_kvasir_unet import experiment
+    from tests.github.interactive_api_director.experiments.tensorflow_mnist import experiment
     experiment.run()
