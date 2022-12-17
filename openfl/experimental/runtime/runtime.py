d894650891a47db55f7aecd1ed7060bef89ab985
Patrick Foley <psfoley@gmail.com>
2022-12-17 01:17:28 +0000

    Create Base Runtime Specification
    Signed-off-by: Patrick Foley <psfoley@gmail.com>

--- openfl/experimental/runtime/runtime.py
+++ openfl/experimental/runtime/runtime.py
@@ -11,3 +11,34 @@ class Runtime:
 
         """
         pass
+
+    @property
+    def aggregator(self):
+        """Returns name of aggregator"""
+        raise NotImplementedError
+
+    @aggregator.setter
+    def aggregator(self, aggregator):
+        """Set Runtime aggregator"""
+        raise NotImplementedError
+
+    @property
+    def collaborators(self):
+        """
+        Return names of collaborators. Don't give direct access to private attributes
+        """
+        raise NotImplementedError
+
+    @collaborators.setter
+    def collaborators(self, collaborators):
+        """Set Runtime collaborators"""
+        raise NotImplementedError
+
+    def execute_task(
+        self, flspec_obj, f, parent_func, instance_snapshot=(), **kwargs
+    ):
+        """
+        Performs the execution of a task as defined by the
+        runtime implementation and underlying backend (single_process, ray, etc)
+        """
+        raise NotImplementedError
