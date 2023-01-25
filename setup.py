20ddd1c55be68a9ce96173743ab24c37e14f53cd
Patrick Foley <patrick.foley@intel.com>
2023-01-25 11:49:01 -0800

    Remove python 3.6 support due to numpy requirements (#713)
    Signed-off-by: Patrick Foley <psfoley@gmail.com>

--- setup.py
+++ setup.py
@@ -157,7 +157,7 @@ setup(
         'tqdm',
     ],
     setup_requires=['grpcio-tools~=1.48.2'],
-    python_requires='>=3.6, <3.11',
+    python_requires='>=3.7, <3.11',
     project_urls={
         'Bug Tracker': 'https://github.com/intel/openfl/issues',
         'Documentation': 'https://openfl.readthedocs.io/en/stable/',
@@ -178,7 +178,6 @@ setup(
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
