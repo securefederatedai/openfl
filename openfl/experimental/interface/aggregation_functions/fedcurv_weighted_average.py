50895d33ef33e629bccf39d19dd0c12f34951217
Mansi Sharma <mansi.sharma@intel.com>
2023-02-28 16:01:29 -0800

    resolve lint errors
    Signed-off-by: Mansi Sharma <mansi.sharma@intel.com>

--- openfl/experimental/interface/aggregation_functions/fedcurv_weighted_average.py
+++ openfl/experimental/interface/aggregation_functions/fedcurv_weighted_average.py
@@ -6,6 +6,7 @@ import torch
 
 from openfl.interface.aggregation_functions.weighted_average import weighted_average as wa
 
+
 def fedcurv_weighted_average(tensors, weights):
     """
     Aggregation function of FedCurv algorithm.
@@ -26,13 +27,18 @@ def fedcurv_weighted_average(tensors, weights):
     if type(tensors[0]) in (dict, collections.OrderedDict):
         tmp_state_dict = {}
         input_state_dict_keys = tensors[0].keys()
-        
+
         # Use diag elements of Fisher matrix
         for key in input_state_dict_keys:
             if (key[-2:] == '_u' or key[-2:] == '_v' or key[-2:] == '_w'):
-                tmp_state_dict[key] = np.sum([tensor[key].detach().cpu() if type(tensor[key]) is torch.Tensor else tensor[key].cpu() for tensor in tensors],axis=0)
+                tmp_state_dict[key] = np.sum([tensor[key].detach().cpu()
+                                             if type(tensor[key]) is torch.Tensor
+                                             else tensor[key].cpu() for tensor in tensors], axis=0)
                 continue
-            tmp_state_dict[key] = np.average([tensor[key].detach().cpu() if type(tensor[key]) is torch.Tensor else tensor[key].cpu() for tensor in tensors], weights=weights, axis=0)
+            tmp_state_dict[key] = np.average([tensor[key].detach().cpu()
+                                             if type(tensor[key]) is torch.Tensor
+                                             else tensor[key].cpu() for tensor in tensors],
+                                             weights=weights, axis=0)
         return tmp_state_dict
     else:
         return wa(tensors, weights)
