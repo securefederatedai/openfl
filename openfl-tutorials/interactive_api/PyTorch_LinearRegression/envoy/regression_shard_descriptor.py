66c6bc231172fe631574f09ea8fbe79c8f4bd7a3
He Dan <dan.h.he@intel.com>
2023-05-20 06:10:03 +0800

    Add PyTorch linear regression example (#808)
    This adds a new tutorial example on distributing a linear regression task over OpenFL cluster.
    
        The model is defined by Pytorch which is able to run over both cpu (by default) and gpu. The dataset is generated by make_regression from sklearn.datasets with pre-defined parameters.
    
        Fixes #797
    
        Co-authored-by: Jiang, Jiaqiu <jiaqiu.jiang@intel.com>
        Signed-off-by: He, Dan H <dan.h.he@intel.com>
        Signed-off-by: Jiang, Jiaqiu <jiaqiu.jiang@intel.com>
        Signed-off-by: Li, Qingqing <qingqing.li@intel.com>
        Signed-off-by: Wang, Le <le3.wang@intel.com>
        Signed-off-by: Wu, Caili <caili.wu@intel.com>
    
    Signed-off-by: He, Dan H <dan.h.he@intel.com>
    Signed-off-by: Patrick Foley <psfoley@gmail.com>

---
+++ openfl-tutorials/interactive_api/PyTorch_LinearRegression/envoy/regression_shard_descriptor.py
@@ -0,0 +1,74 @@
+# Copyright (C) 2020-2022 Intel Corporation
+# SPDX-License-Identifier: Apache-2.0
+"""Noisy-Sin Shard Descriptor."""
+
+from typing import List
+
+import numpy as np
+import torch
+from sklearn.datasets import make_regression
+from sklearn.model_selection import train_test_split
+
+from openfl.interface.interactive_api.shard_descriptor import ShardDescriptor
+
+
+class RegressionShardDescriptor(ShardDescriptor):
+    """Regression Shard descriptor class."""
+
+    def __init__(self, rank_worldsize: str = '1, 1', **kwargs) -> None:
+        """
+        Initialize Regression Data Shard Descriptor.
+
+        This Shard Descriptor generate random regression data with some gaussian centered noise
+        using make_regression method from sklearn.datasets.
+        Shards data across participants using rank and world size.
+        """
+
+        self.rank, self.worldsize = tuple(int(num) for num in rank_worldsize.split(','))
+        X_train, y_train, X_test, y_test = self.generate_data()
+        self.data_by_type = {
+            'train': np.concatenate((X_train, y_train[:, None]), axis=1),
+            'val': np.concatenate((X_test, y_test[:, None]), axis=1)
+        }
+
+    def generate_data(self):
+        """Generate regression dataset with predefined params."""
+        x, y = make_regression(n_samples=1000, n_features=1, noise=14, random_state=24)
+        X_train, X_test, y_train, y_test = train_test_split(x, y, random_state=24)
+        self.data = np.concatenate((x, y[:, None]), axis=1)
+        return X_train, y_train, X_test, y_test
+
+    def get_shard_dataset_types(self) -> List[str]:
+        """Get available shard dataset types."""
+        return list(self.data_by_type)
+
+    def get_dataset(self, dataset_type='train'):
+        """Return a shard dataset by type."""
+        if dataset_type not in self.data_by_type:
+            raise Exception(f'Incorrect dataset type: {dataset_type}')
+
+        if dataset_type in ['train', 'val']:
+            return torch.tensor(
+                self.data_by_type[dataset_type][self.rank - 1::self.worldsize],
+                dtype=torch.float32
+            )
+        else:
+            raise ValueError
+
+    @property
+    def sample_shape(self) -> List[str]:
+        """Return the sample shape info."""
+        (*x, _) = self.data[0]
+        return [str(i) for i in np.array(x, ndmin=1).shape]
+
+    @property
+    def target_shape(self) -> List[str]:
+        """Return the target shape info."""
+        (*_, y) = self.data[0]
+        return [str(i) for i in np.array(y, ndmin=1).shape]
+
+    @property
+    def dataset_description(self) -> str:
+        """Return the dataset description."""
+        return (f'Regression dataset, shard number {self.rank}'
+                f' out of {self.worldsize}')
