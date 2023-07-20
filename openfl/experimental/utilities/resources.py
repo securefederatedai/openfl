e899e5c5098afaf1948eb821e0450027c35fabf8
Parth Mandaliya <parthx.mandaliya@intel.com>
2023-07-21 02:20:49 +0530

    Delete openfl/experimental/utilities/utils.py, and removed torch depency from openfl/experimental/utilities/resources.py
    Signed-off-by: Parth Mandaliya <parthx.mandaliya@intel.com>
    Signed-off-by: Parth Mandaliya <parth.mandaliya.007@gmail.com>

--- openfl/experimental/utilities/resources.py
+++ openfl/experimental/utilities/resources.py
@@ -3,9 +3,31 @@
 
 """openfl.experimental.utilities.resources module."""
 
-from torch.cuda import device_count
+from logging import getLogger
+from subprocess import run, PIPE
+
+logger = getLogger(__name__)
 
 
-def get_number_of_gpus():
-    # TODO remove pytorch dependency
-    return device_count()
+def get_number_of_gpus() -> int:
+    """
+    Returns number of NVIDIA GPUs attached to the machine.
+
+    Args:
+        None
+    Returns:
+        int: Number of NVIDIA GPUs
+    """
+    # Execute the nvidia-smi command.
+    command = "nvidia-smi --query-gpu=name --format=csv,noheader"
+    op = run(command.strip().split(), shell=False, stdout=PIPE, stderr=PIPE)
+
+    # If returncode is 0 then command ran successfully
+    if op.returncode is 0:
+        stdout = op.stdout.decode().strip()
+
+        return len(stdout.split("\n"))
+    else:
+        stderr = op.stderr.decode().strip()
+        logger.warning(f"No GPUs found! If this is a mistake please check nvidia-smi error {stderr}")
+        return 0
