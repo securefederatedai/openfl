fb0e1e1100d2fdde68c25c297f5893ed5415035e
ParthM-GitHub <parthx.mandaliya@intel.com>
2023-07-25 15:24:49 +0530

    Removed torch dependency
    Removed torch dependency from get_number_of_gpus() function
    
    Signed-off-by: Parth Mandaliya <parthx.mandaliya@intel.com>

--- openfl/experimental/utilities/resources.py
+++ openfl/experimental/utilities/resources.py
@@ -6,6 +6,21 @@
 from torch.cuda import device_count
 
 
-def get_number_of_gpus():
-    # TODO remove pytorch dependency
-    return device_count()
+def get_number_of_gpus() -> int:
+    """
+    Returns number of NVIDIA GPUs attached to the machine.
+
+    Args:
+        None
+    Returns:
+        int: Number of NVIDIA GPUs
+    """
+    # Execute the nvidia-smi command.
+    command = "nvidia-smi --list-gpus"
+    try:
+        op = run(command.strip().split(), shell=False, stdout=PIPE, stderr=PIPE)
+        stdout = op.stdout.decode().strip()
+        return len(stdout.split("\n"))
+    except FileNotFoundError:
+        logger.warning(f"No GPUs found! If this is a mistake please try running {command} manually.")
+        return 0
