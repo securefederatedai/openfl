76abc9aacae44419dc070be807437780cdae8883
Parth Mandaliya <parthx.mandaliya@intel.com>
2023-08-10 13:37:24 +0530

    Added color escape sequences to datastore cli localruntime and workspace testcases
    Signed-off-by: Parth Mandaliya <parthx.mandaliya@intel.com>
    Signed-off-by: Parth Mandaliya <parth.mandaliya.007@gmail.com>

--- tests/github/experimental/testflow_datastore_cli.py
+++ tests/github/experimental/testflow_datastore_cli.py
@@ -176,12 +176,18 @@ def validate_datastore_cli(flow_obj, expected_flow_steps, num_rounds):
     validate_flow_error = []
 
     verify_stdout = {
-        "start": "Testing FederatedFlow - Starting Test for Dataflow and CLI Functionality\n",
-        "aggregated_model_validation": "Performing aggregated model validation for collaborator\n",
-        "train": "Train the model\n",
-        "local_model_validation": "Doing local model validation for collaborator\n",
-        "join": "Executing join\n",
-        "end": "This is the end of the flow\n",
+        "start":
+            "\x1b[94mTesting FederatedFlow - Starting Test for Dataflow"
+            + " and CLI Functionality\x1b[0m\x1b[94m\n\x1b[0m\n",
+        "aggregated_model_validation":
+            "\x1b[94mPerforming aggregated model validation for"
+            + " collaborator\x1b[0m\x1b[94m\n\x1b[0m\n",
+        "train": "\x1b[94mTrain the model\x1b[0m\x1b[94m\n\x1b[0m\n",
+        "local_model_validation":
+            "\x1b[94mDoing local model validation for collaborator"
+            + "\x1b[0m\x1b[94m\n\x1b[0m\n",
+        "join": "\x1b[94mExecuting join\x1b[0m\x1b[94m\n\x1b[0m\n",
+        "end": "\x1b[94mThis is the end of the flow\x1b[0m\x1b[94m\n\x1b[0m\n",
     }
 
     # fetch data from metaflow
@@ -263,12 +269,14 @@ def validate_datastore_cli(flow_obj, expected_flow_steps, num_rounds):
     if validate_flow_error:
         display_validate_errors(validate_flow_error)
     else:
-        print(f"""{Bcolors.OKGREEN}\n**** Summary of internal flow testing ****
+        print(
+            f"""{Bcolors.OKGREEN}\n**** Summary of internal flow testing ****
               No issues found and below are the tests that ran successfully
               1. Datastore steps and expected steps are matching
               2. Task stdout and task stderr verified through metaflow cli is as expected
-              3. Number of tasks are aligned with number of rounds and number\
-                  of collaborators {Bcolors.ENDC}""")
+              3. Number of tasks are aligned with number of rounds and number
+                 of collaborators {Bcolors.ENDC}"""
+        )
 
 
 def display_validate_errors(validate_flow_error):
