9dd5819ec9956a39a600b8528a5724ec36de7201
ParthM-GitHub <parthx.mandaliya@intel.com>
2023-08-11 19:50:20 +0530

    Update utils.py
    Updated get_number_of_gpus function

--- openfl/experimental/utilities/utils.py
+++ openfl/experimental/utilities/utils.py
@@ -1,8 +1,29 @@
 # Copyright (C) 2020-2023 Intel Corporation
 # SPDX-License-Identifier: Apache-2.0
+"""openfl.experimental.utilities.resources module."""
 
-from torch.cuda import device_count
+from logging import getLogger
+from subprocess import run, PIPE
+
+logger = getLogger(__name__)
 
 
 def get_number_of_gpus() -> int:
-    return device_count()
+    """
+    Returns number of NVIDIA GPUs attached to the machine.
+
+    Args:
+        None
+    Returns:
+        int: Number of NVIDIA GPUs
+    """
+    # Execute the nvidia-smi command.
+    command = "nvidia-smi --list-gpus"
+    try:
+        op = run(command.strip().split(), shell=False, stdout=PIPE, stderr=PIPE)
+        stdout = op.stdout.decode().strip()
+        return len(stdout.split("\n"))
+    except FileNotFoundError:
+        logger.warning(f"No GPUs found! If this is a mistake please try running '{command}' "
+                       + "manually.")
+        return 0
