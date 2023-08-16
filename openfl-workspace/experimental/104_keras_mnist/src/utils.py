e45eab794d8aeb2f3b20dda9c7da8ab8ba1352ed
ParthM-GitHub <parthx.mandaliya@intel.com>
2023-08-16 19:01:39 +0530

    Updating import statement
    In 104 keras workspace utils.py updating import SummaryWriter statement from torch to tensorflow
    
    Signed-off-by: Parth Mandaliya <parthx.mandaliya@intel.com>
    Signed-off-by: Parth Mandaliya <parth.mandaliya.007@gmail.com>

--- openfl-workspace/experimental/104_keras_mnist/src/utils.py
+++ openfl-workspace/experimental/104_keras_mnist/src/utils.py
@@ -1,7 +1,7 @@
 # Copyright (C) 2020-2021 Intel Corporation
 # SPDX-License-Identifier: Apache-2.0
 
-from torch.utils.tensorboard import SummaryWriter
+from tensorflow.summary import SummaryWriter
 
 
 writer = None
