3b69cf9befe377cbd21f383640a62a9b798db64e
porteratzo <omar.alfonso.montoya@hotmail.com>
2023-10-30 07:57:07 -0700

    f

--- openfl-tutorials/experimental/setup_env.sh
+++ openfl-tutorials/experimental/setup_env.sh
@@ -5,6 +5,6 @@ python3.8 -m venv ~/workspace/$vname
 source ~/workspace/$vname/bin/activate
 pip install -U pip --no-cache
 pip install -r reqstorch.txt
-pip install -r reqs.txt
-cd ../..
-pip install .
+#pip install -r reqs.txt
+#cd ../..
+#pip install .
