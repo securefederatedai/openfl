ea919a9988cc4926707eb1f7764b721d936041fd
porteratzo <omar.alfonso.montoya@hotmail.com>
2023-10-30 08:56:45 -0700

    d

--- openfl-tutorials/experimental/setup_env.sh
+++ openfl-tutorials/experimental/setup_env.sh
@@ -1,7 +1,8 @@
 conda remove -n speed --all -y
-conda create -n speed python==3.8 pytorch torchvision torchaudio pytorch-cuda=11.8 -c pytorch -c nvidia -y
+conda create -n speed python=3.8 pytorch torchvision torchaudio pytorch-cuda=11.8 -c pytorch -c nvidia -y
 source /home/oamontoy/miniconda3/bin/activate speed 
 conda install -c nvidia cuda-compiler -y
 pip install -U pip setuptools
 pip install -r reqs.txt
 pip install ../../.
+echo conda activate speed
