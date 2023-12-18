1739720c5a07841059ff2b8d2aa0e55498f8780d
Mansi Sharma <mansi.sharma@intel.com>
2023-12-18 14:14:23 -0800

    Adding fedcurv with example
    Signed-off-by: Mansi Sharma <mansi.sharma@intel.com>

--- openfl/experimental/interface/aggregation_functions/fedprox.py
+++ openfl/experimental/interface/aggregation_functions/fedprox.py
@@ -1,28 +1,30 @@
 """openfl.experimental.interface.aggregation_functions.fedprox package."""
-
 import numpy as np
+from typing import List
 
-from .weighted_average import weighted_average, WeightedAverage
+from .fedavg import FedAvg, weighted_average
 
 
-class FedProx(WeightedAverage):
-    """Weighted average aggregation."""
+class FedProxAgg(FedAvg):
+    """FedProx aggregation.
+    
+    A representation of FedAvg with inclusion of the
+    proximal term for heterogeneous data distributions
+    
+    FedProx paper: https://arxiv.org/pdf/1812.06127.pdf
+    """
+
     def __init__(self, **kwargs) -> None:
         super().__init__(**kwargs)
 
     def __repr__(self) -> str:
         """Compute a string representation of the function."""
-        rep = "FedProx"
+        rep = "FedProx Aggregation Function"
         return rep
 
-    def aggregate_metrics(self, metrics, train_weights, test_weights):
+    def aggregate_metrics(self, metrics, weights) -> List[np.ndarray]:
         """Weighted average of loss and accuracy metrics"""
-        agg_model_loss_list, agg_model_accuracy_list = metrics
-        aggregated_model_training_loss = weighted_average(agg_model_loss_list, train_weights)
-        aggregated_model_test_accuracy = weighted_average(agg_model_accuracy_list, test_weights)
-
-        return (aggregated_model_training_loss, aggregated_model_test_accuracy)
-
-
-
- 
+        agg_metrics = []
+        for idx,metric in enumerate(metrics):
+            agg_metrics.append(weighted_average(metric, weights=weights[idx]))
+        return agg_metrics
