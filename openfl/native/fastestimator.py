b940693d1891233ab5a526248d9333da46f5bc97
manuelhsantana <manuel.santana.castolo@intel.com>
2024-01-03 15:37:35 -0800

    Added native module docstrings for API documentation
    Signed-off-by: manuelhsantana <manuel.santana.castolo@intel.com>
    Signed-off-by: porteratzo <omar.alfonso.montoya@hotmail.com>

--- openfl/native/fastestimator.py
+++ openfl/native/fastestimator.py
@@ -15,10 +15,19 @@ from openfl.utilities.split import split_tensor_dict_for_holdouts
 
 
 class FederatedFastEstimator:
-    """A wrapper for fastestimator.estimator that allows running in federated mode."""
+    """
+    A wrapper for fastestimator.estimator that allows running in federated mode.
+    """
 
     def __init__(self, estimator, override_config: dict = None, **kwargs):
-        """Initialize."""
+        """
+        Initializes a new instance of the FederatedFastEstimator class.
+
+            Args:
+                estimator : The FastEstimator to be used.
+                override_config (dict, optional): A dictionary to override the default configuration. Defaults to None.
+                **kwargs : Additional keyword arguments.
+        """
         self.estimator = estimator
         self.logger = getLogger(__name__)
         fx.init(**kwargs)
@@ -26,7 +35,9 @@ class FederatedFastEstimator:
             fx.update_plan(override_config)
 
     def fit(self):
-        """Run the estimator."""
+        """
+        Runs the estimator in federated mode.
+        """
         import fastestimator as fe
         from fastestimator.trace.io.best_model_saver import BestModelSaver
         from sys import path
@@ -154,7 +165,19 @@ class FederatedFastEstimator:
 
 
 def split_data(train, eva, test, rank, collaborator_count):
-    """Split data into N parts, where N is the collaborator count."""
+    """
+    Split data into N parts, where N is the collaborator count.
+
+    Args:
+        train : The training data.
+        eva : The evaluation data.
+        test : The testing data.
+        rank (int): The rank of the current collaborator.
+        collaborator_count (int): The total number of collaborators.
+        
+    Returns:
+        tuple: The training, evaluation, and testing data for the current collaborator.
+    """
     if collaborator_count == 1:
         return train, eva, test
 
