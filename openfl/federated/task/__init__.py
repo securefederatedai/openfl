2018e1f0297be9569621201ffe5f9eaba4608c81
kta-intel <kevin.ta@intel.com>
2024-06-06 14:47:39 -0700

    minor fix to find_spec()
    Signed-off-by: kta-intel <kevin.ta@intel.com>

--- openfl/federated/task/__init__.py
+++ openfl/federated/task/__init__.py
@@ -9,17 +9,17 @@ from warnings import simplefilter
 
 with catch_warnings():
     simplefilter(action='ignore', category=FutureWarning)
-    if importlib.util.find_spec('tensorflow') is not None:
+    if importlib.util.find_spec('tensorflow'):
         # ignore deprecation warnings in command-line interface
         import tensorflow  # NOQA
 
 from .runner import TaskRunner  # NOQA
 
-if importlib.util.find_spec('tensorflow') is not None:
+if importlib.util.find_spec('tensorflow'):
     from .runner_tf import TensorFlowTaskRunner  # NOQA
     from .runner_tf import TensorFlowTaskRunner_v1  # NOQA
     from .runner_keras import KerasTaskRunner  # NOQA
     from .fl_model import FederatedModel  # NOQA
-if importlib.util.find_spec('torch') is not None:
+if importlib.util.find_spec('torch'):
     from .runner_pt import PyTorchTaskRunner  # NOQA
     from .fl_model import FederatedModel  # NOQA
