88340f9d5a57839652f829d4e618212855fa48a0
Ishant Thakare <ishantrog752@gmail.com>
2024-06-12 13:42:02 +0530

    updated participant names & added testcase to validate participant names
    Signed-off-by: Ishant Thakare <ishantrog752@gmail.com>

--- openfl/experimental/interface/participants.py
+++ openfl/experimental/interface/participants.py
@@ -9,7 +9,7 @@ class Participant:
 
     def __init__(self, name: str = ""):
         self.private_attributes = {}
-        self._name = name.lower()
+        self._name = name
 
     @property
     def name(self):
@@ -17,7 +17,7 @@ class Participant:
 
     @name.setter
     def name(self, name: str):
-        self._name = name.lower()
+        self._name = name
 
     def private_attributes(self, attrs: Dict[str, Any]) -> None:
         """
