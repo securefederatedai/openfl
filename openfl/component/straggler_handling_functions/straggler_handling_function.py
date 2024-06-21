f1756572aed9a4786ec6882a3b74f7fe76dbbcf5
Parth Mandaliya <parth.mandaliya.007@gmail.com>
2024-06-21 22:00:58 +0530

    Only time based straggler handling policies require timer thread, removing start_straggler_cutoff_timer function from parent class StragglerHandlingFunction
    Signed-off-by: Parth Mandaliya <parth.mandaliya.007@gmail.com>

--- openfl/component/straggler_handling_functions/straggler_handling_function.py
+++ openfl/component/straggler_handling_functions/straggler_handling_function.py
@@ -5,8 +5,6 @@
 
 from abc import ABC
 from abc import abstractmethod
-from typing import Callable
-from logging import Logger
 
 
 class StragglerHandlingFunction(ABC):
@@ -18,22 +16,5 @@ class StragglerHandlingFunction(ABC):
         Determines whether it is time to end the round early.
         Returns:
             bool
-        """
-        raise NotImplementedError
-
-    @abstractmethod
-    def start_straggler_cutoff_timer(
-        self, callback: Callable, logger: Logger
-    ) -> None:
-        """
-        Start timer.
-
-        Args:
-            callback: Callable
-                Callback function for when straggler_cutoff_time elapses
-            logger: Logger
-
-        Returns:
-            None
         """
         raise NotImplementedError
