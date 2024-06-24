eeafaf5b008f704c34163d78c788ee4904fbbdf8
Patrick Foley <patrick.foley@intel.com>
2024-06-24 13:42:27 -0700

    Allow `feature_shape` to be passed to `fx plan initialize` (#983)
    * Remove need for aggregator to have dataset for model weight initialization
    
    Signed-off-by: Patrick Foley <psfoley@gmail.com>
    
    * Remove extra print arguments
    
    Signed-off-by: Patrick Foley <psfoley@gmail.com>
    
    * Address review comments
    
    Signed-off-by: Patrick Foley <psfoley@gmail.com>
    
    * Remove extraneous print statement
    
    Signed-off-by: Patrick Foley <psfoley@gmail.com>
    
    ---------
    
    Signed-off-by: Patrick Foley <psfoley@gmail.com>

---
+++ openfl/utilities/mocks.py
@@ -0,0 +1,12 @@
+# Copyright (C) 2020-2024 Intel Corporation
+# SPDX-License-Identifier: Apache-2.0
+"""Mock objects to eliminate extraneous dependencies"""
+
+
+class MockDataLoader:
+    """Placeholder dataloader for when data is not available"""
+    def __init__(self, feature_shape):
+        self.feature_shape = feature_shape
+
+    def get_feature_shape(self):
+        return self.feature_shape
