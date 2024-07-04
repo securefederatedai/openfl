1294bb6517644a633fe2bdc50a4cc15e9ebac6c2
manuelhsantana <manuel.santana.castolo@intel.com>
2024-07-04 13:07:22 -0700

    Update cryptography functions docstring for fix lint warnings
    Signed-off-by: manuelhsantana <manuel.santana.castolo@intel.com>
    Signed-off-by: porteratzo <omar.alfonso.montoya@hotmail.com>

--- openfl/cryptography/io.py
+++ openfl/cryptography/io.py
@@ -1,6 +1,5 @@
 # Copyright (C) 2020-2023 Intel Corporation
 # SPDX-License-Identifier: Apache-2.0
-
 """Cryptography IO utilities."""
 
 import os
@@ -13,16 +12,15 @@ from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.primitives.asymmetric import rsa
 from cryptography.hazmat.primitives.asymmetric.rsa import RSAPrivateKey
 from cryptography.hazmat.primitives.serialization import load_pem_private_key
-from cryptography.x509.base import Certificate
-from cryptography.x509.base import CertificateSigningRequest
+from cryptography.x509.base import Certificate, CertificateSigningRequest
 
 
 def read_key(path: Path) -> RSAPrivateKey:
     """Reads a private key from a file.
-    
+
     Args:
         path (Path): The path to the file containing the private key.
-        
+
     Returns:
         RSAPrivateKey: The private key.
     """
@@ -37,28 +35,29 @@ def read_key(path: Path) -> RSAPrivateKey:
 
 def write_key(key: RSAPrivateKey, path: Path) -> None:
     """Writes a private key to a file.
-    
+
     Args:
         key (RSAPrivateKey): The private key to write.
         path (Path): The path to the file to write the private key to.
     """
+
     def key_opener(path, flags):
         return os.open(path, flags, mode=0o600)
 
     with open(path, 'wb', opener=key_opener) as f:
-        f.write(key.private_bytes(
-            encoding=serialization.Encoding.PEM,
-            format=serialization.PrivateFormat.TraditionalOpenSSL,
-            encryption_algorithm=serialization.NoEncryption()
-        ))
+        f.write(
+            key.private_bytes(
+                encoding=serialization.Encoding.PEM,
+                format=serialization.PrivateFormat.TraditionalOpenSSL,
+                encryption_algorithm=serialization.NoEncryption()))
 
 
 def read_crt(path: Path) -> Certificate:
     """Reads a signed TLS certificate from a file.
-    
+
     Args:
         path (Path): The path to the file containing the certificate.
-        
+
     Returns:
         Certificate: The TLS certificate.
     """
@@ -75,21 +74,21 @@ def write_crt(certificate: Certificate, path: Path) -> None:
     """Writes a cryptography certificate / CSR to a file.
 
     Args:
-        certificate (Certificate): cryptography csr / certificate object to write.
+        certificate (Certificate): cryptography csr / certificate object to
+            write.
         path (Path): The path to the file to write the certificate to.
     """
     with open(path, 'wb') as f:
         f.write(certificate.public_bytes(
-            encoding=serialization.Encoding.PEM,
-        ))
+            encoding=serialization.Encoding.PEM, ))
 
 
 def read_csr(path: Path) -> Tuple[CertificateSigningRequest, str]:
     """Reads a Certificate Signing Request (CSR) from a file.
-    
+
     Args:
         path (Path): The path to the file containing the CSR.
-        
+
     Returns:
         Tuple[CertificateSigningRequest, str]: The CSR and its hash.
     """
@@ -104,16 +103,16 @@ def read_csr(path: Path) -> Tuple[CertificateSigningRequest, str]:
 
 def get_csr_hash(certificate: CertificateSigningRequest) -> str:
     """Computes the SHA-384 hash of a certificate.
-    
+
     Args:
-        certificate (CertificateSigningRequest): The certificate to compute the hash of.
-        
+        certificate (CertificateSigningRequest): The certificate to compute
+            the hash of.
+
     Returns:
         str: The SHA-384 hash of the certificate.
     """
     hasher = sha384()
     encoded_bytes = certificate.public_bytes(
-        encoding=serialization.Encoding.PEM,
-    )
+        encoding=serialization.Encoding.PEM, )
     hasher.update(encoded_bytes)
     return hasher.hexdigest()
