c6a5fced013a3642203f50f55a36d691793c6474
manuelhsantana <manuel.santana.castolo@intel.com>
2024-07-03 22:28:40 -0700

    Update component imports docstring for fix lint warnings
    Signed-off-by: manuelhsantana <manuel.santana.castolo@intel.com>
    Signed-off-by: porteratzo <omar.alfonso.montoya@hotmail.com>

--- openfl/component/straggler_handling_functions/percentage_based_straggler_handling.py
+++ openfl/component/straggler_handling_functions/percentage_based_straggler_handling.py
@@ -1,7 +1,9 @@
 # Copyright (C) 2020-2023 Intel Corporation
 # SPDX-License-Identifier: Apache-2.0
 """Percentage based Straggler Handling function."""
-from openfl.component.straggler_handling_functions import StragglerHandlingFunction
+from openfl.component.straggler_handling_functions import (
+    StragglerHandlingFunction,
+)
 
 
 class PercentageBasedStragglerHandling(StragglerHandlingFunction):
