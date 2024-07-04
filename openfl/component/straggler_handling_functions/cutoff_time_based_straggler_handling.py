5a1d7a2e48f034762fa8d7216d5c2bb807a9b5c6
manuelhsantana <manuel.santana.castolo@intel.com>
2024-07-03 21:19:44 -0700

    Update component module functions docstrings for fix lint warnings

--- openfl/component/straggler_handling_functions/cutoff_time_based_straggler_handling.py
+++ openfl/component/straggler_handling_functions/cutoff_time_based_straggler_handling.py
@@ -1,29 +1,32 @@
 # Copyright (C) 2020-2023 Intel Corporation
 # SPDX-License-Identifier: Apache-2.0
-
 """Cutoff time based Straggler Handling function."""
-import numpy as np
 import time
 
-from openfl.component.straggler_handling_functions import StragglerHandlingFunction
+import numpy as np
+
+from openfl.component.straggler_handling_functions import (
+    StragglerHandlingFunction,
+)
 
 
 class CutoffTimeBasedStragglerHandling(StragglerHandlingFunction):
     """Cutoff time based Straggler Handling function."""
-    
-    def __init__(
-        self,
-        round_start_time=None,
-        straggler_cutoff_time=np.inf,
-        minimum_reporting=1,
-        **kwargs
-    ):
+
+    def __init__(self,
+                 round_start_time=None,
+                 straggler_cutoff_time=np.inf,
+                 minimum_reporting=1,
+                 **kwargs):
         """Initialize a CutoffTimeBasedStragglerHandling object.
 
         Args:
-            round_start_time (optional): The start time of the round. Defaults to None.
-            straggler_cutoff_time (float, optional): The cutoff time for stragglers. Defaults to np.inf.
-            minimum_reporting (int, optional): The minimum number of collaborators that should report. Defaults to 1.
+            round_start_time (optional): The start time of the round. Defaults
+                to None.
+            straggler_cutoff_time (float, optional): The cutoff time for
+                stragglers. Defaults to np.inf.
+            minimum_reporting (int, optional): The minimum number of
+                collaborators that should report. Defaults to 1.
             **kwargs: Variable length argument list.
         """
         self.round_start_time = round_start_time
@@ -43,23 +46,30 @@ class CutoffTimeBasedStragglerHandling(StragglerHandlingFunction):
         """Check if the minimum number of collaborators have reported.
 
         Args:
-            num_collaborators_done (int): The number of collaborators that have reported.
+            num_collaborators_done (int): The number of collaborators that
+                have reported.
 
         Returns:
-            bool: True if the minimum number of collaborators have reported, False otherwise.
+            bool: True if the minimum number of collaborators have reported,
+                False otherwise.
         """
         return num_collaborators_done >= self.minimum_reporting
 
-    def straggler_cutoff_check(self, num_collaborators_done, all_collaborators=None):
+    def straggler_cutoff_check(self,
+                               num_collaborators_done,
+                               all_collaborators=None):
         """Check if the straggler cutoff conditions are met.
 
         Args:
-            num_collaborators_done (int): The number of collaborators that have reported.
-            all_collaborators (optional): All the collaborators. Defaults to None.
+            num_collaborators_done (int): The number of collaborators that
+                have reported.
+            all_collaborators (optional): All the collaborators. Defaults to
+                None.
 
         Returns:
-            bool: True if the straggler cutoff conditions are met, False otherwise.
+            bool: True if the straggler cutoff conditions are met, False
+                otherwise.
         """
-        cutoff = self.straggler_time_expired() and self.minimum_collaborators_reported(
-            num_collaborators_done)
+        cutoff = self.straggler_time_expired(
+        ) and self.minimum_collaborators_reported(num_collaborators_done)
         return cutoff
