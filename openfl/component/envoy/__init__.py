b52bd3166d944e6cadd45c1f77bf28d18e91a636
manuelhsantana <manuel.santana.castolo@intel.com>
2024-07-03 21:19:44 -0700

    Update component module functions docstrings for fix lint warnings

--- openfl/component/envoy/__init__.py
+++ openfl/component/envoy/__init__.py
@@ -1,6 +1,5 @@
 # Copyright (C) 2020-2023 Intel Corporation
 # SPDX-License-Identifier: Apache-2.0
-
 """Envoy package."""
 
 from .envoy import Envoy
--- openfl/component/straggler_handling_functions/__init__.py
+++ openfl/component/straggler_handling_functions/__init__.py
@@ -1,12 +1,16 @@
 # Copyright (C) 2020-2023 Intel Corporation
 # SPDX-License-Identifier: Apache-2.0
-
 """Straggler Handling functions package."""
 
+from .cutoff_time_based_straggler_handling import (
+    CutoffTimeBasedStragglerHandling,
+)
+from .percentage_based_straggler_handling import (
+    PercentageBasedStragglerHandling,
+)
 from .straggler_handling_function import StragglerHandlingFunction
-from .cutoff_time_based_straggler_handling import CutoffTimeBasedStragglerHandling
-from .percentage_based_straggler_handling import PercentageBasedStragglerHandling
 
-__all__ = ['CutoffTimeBasedStragglerHandling',
-           'PercentageBasedStragglerHandling',
-           'StragglerHandlingFunction']
+__all__ = [
+    'CutoffTimeBasedStragglerHandling', 'PercentageBasedStragglerHandling',
+    'StragglerHandlingFunction'
+]
