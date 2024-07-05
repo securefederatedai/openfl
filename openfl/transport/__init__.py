e88fd3657f903b785c73bb75004813ee1004ba07
manuelhsantana <manuel.santana.castolo@intel.com>
2024-07-04 18:12:04 -0700

    Update transport functions docstring for fix lint warnings

--- openfl/transport/__init__.py
+++ openfl/transport/__init__.py
@@ -1,6 +1,5 @@
 # Copyright (C) 2020-2023 Intel Corporation
 # SPDX-License-Identifier: Apache-2.0
-
 """openfl.transport package."""
 
 from .grpc import AggregatorGRPCClient
--- openfl/transport/grpc/__init__.py
+++ openfl/transport/grpc/__init__.py
@@ -1,6 +1,5 @@
 # Copyright (C) 2020-2023 Intel Corporation
 # SPDX-License-Identifier: Apache-2.0
-
 """openfl.transport.grpc package."""
 
 from .aggregator_client import AggregatorGRPCClient
