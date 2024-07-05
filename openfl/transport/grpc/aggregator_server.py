efa4e8ae0af9ed30c63cebbd0620b2795b3b5800
manuelhsantana <manuel.santana.castolo@intel.com>
2024-07-04 18:12:04 -0700

    Update transport functions docstring for fix lint warnings
    Signed-off-by: manuelhsantana <manuel.santana.castolo@intel.com>

--- openfl/transport/grpc/aggregator_server.py
+++ openfl/transport/grpc/aggregator_server.py
@@ -1,6 +1,5 @@
 # Copyright (C) 2020-2023 Intel Corporation
 # SPDX-License-Identifier: Apache-2.0
-
 """AggregatorGRPCServer module."""
 
 import logging
@@ -25,18 +24,23 @@ logger = logging.getLogger(__name__)
 
 
 class AggregatorGRPCServer(aggregator_pb2_grpc.AggregatorServicer):
-    """gRPC server class for the Aggregator.
-    
-    This class implements a gRPC server for the Aggregator, allowing it to communicate with collaborators.
+    """GRPC server class for the Aggregator.
+
+    This class implements a gRPC server for the Aggregator, allowing it to
+    communicate with collaborators.
 
     Attributes:
         aggregator (Aggregator): The aggregator that this server is serving.
         uri (str): The URI that the server is serving on.
         tls (bool): Whether to use TLS for the connection.
-        disable_client_auth (bool): Whether to disable client-side authentication.
-        root_certificate (str): The path to the root certificate for the TLS connection.
-        certificate (str): The path to the server's certificate for the TLS connection.
-        private_key (str): The path to the server's private key for the TLS connection.
+        disable_client_auth (bool): Whether to disable client-side
+            authentication.
+        root_certificate (str): The path to the root certificate for the TLS
+            connection.
+        certificate (str): The path to the server's certificate for the TLS
+            connection.
+        private_key (str): The path to the server's private key for the TLS
+            connection.
         server (grpc.Server): The gRPC server.
         server_credentials (grpc.ServerCredentials): The server's credentials.
     """
@@ -53,14 +57,19 @@ class AggregatorGRPCServer(aggregator_pb2_grpc.AggregatorServicer):
         """Initialize the AggregatorGRPCServer.
 
         Args:
-            aggregator (Aggregator): The aggregator that this server is serving.
+            aggregator (Aggregator): The aggregator that this server is
+                serving.
             agg_port (int): The port that the server is serving on.
             tls (bool): Whether to use TLS for the connection.
-            disable_client_auth (bool): Whether to disable client-side authentication.
-            root_certificate (str): The path to the root certificate for the TLS connection.
-            certificate (str): The path to the server's certificate for the TLS connection.
-            private_key (str): The path to the server's private key for the TLS connection.
-            **kwargs: Additional keyword arguments.   
+            disable_client_auth (bool): Whether to disable client-side
+                authentication.
+            root_certificate (str): The path to the root certificate for the
+                TLS connection.
+            certificate (str): The path to the server's certificate for the
+                TLS connection.
+            private_key (str): The path to the server's private key for the
+                TLS connection.
+            **kwargs: Additional keyword arguments.
         """
         self.aggregator = aggregator
         self.uri = f'[::]:{agg_port}'
@@ -77,18 +86,21 @@ class AggregatorGRPCServer(aggregator_pb2_grpc.AggregatorServicer):
     def validate_collaborator(self, request, context):
         """Validate the collaborator.
 
-        This method checks that the collaborator who sent the request is authorized to do so.
-        
+        This method checks that the collaborator who sent the request is
+        authorized to do so.
+
         Args:
-            request (aggregator_pb2.MessageHeader): The request from the collaborator.
+            request (aggregator_pb2.MessageHeader): The request from the
+                collaborator.
             context (grpc.ServicerContext): The context of the request.
 
         Raises:
-            grpc.RpcError: If the collaborator or collaborator certificate is not authorized.
+            grpc.RpcError: If the collaborator or collaborator certificate is
+                not authorized.
         """
         if self.tls:
-            common_name = context.auth_context()[
-                'x509_common_name'][0].decode('utf-8')
+            common_name = context.auth_context()['x509_common_name'][0].decode(
+                'utf-8')
             collaborator_common_name = request.header.sender
             if not self.aggregator.valid_collaborator_cn_and_id(
                     common_name, collaborator_common_name):
@@ -102,10 +114,12 @@ class AggregatorGRPCServer(aggregator_pb2_grpc.AggregatorServicer):
     def get_header(self, collaborator_name):
         """Compose and return MessageHeader.
 
-        This method creates a MessageHeader for a message to the specified collaborator.
+        This method creates a MessageHeader for a message to the specified
+        collaborator.
 
         Args:
-            collaborator_name (str): The name of the collaborator to send the message to.
+            collaborator_name (str): The name of the collaborator to send the
+                message to.
 
         Returns:
             aggregator_pb2.MessageHeader: The header for the message.
@@ -114,36 +128,36 @@ class AggregatorGRPCServer(aggregator_pb2_grpc.AggregatorServicer):
             sender=self.aggregator.uuid,
             receiver=collaborator_name,
             federation_uuid=self.aggregator.federation_uuid,
-            single_col_cert_common_name=self.aggregator.single_col_cert_common_name
-        )
+            single_col_cert_common_name=self.aggregator.
+            single_col_cert_common_name)
 
     def check_request(self, request):
         """Validate request header matches expected values.
 
-        This method checks that the request is valid and was sent by an authorized collaborator.
+        This method checks that the request is valid and was sent by an
+            authorized collaborator.
 
         Args:
-            request (aggregator_pb2.MessageHeader): Request sent from a collaborator that requires validation.
-        
+            request (aggregator_pb2.MessageHeader): Request sent from a
+                collaborator that requires validation.
+
         Raises:
-            ValueError: If the request is not valid.        
+            ValueError: If the request is not valid.
         """
         # TODO improve this check. the sender name could be spoofed
-        check_is_in(request.header.sender, self.aggregator.authorized_cols, self.logger)
+        check_is_in(request.header.sender, self.aggregator.authorized_cols,
+                    self.logger)
 
         # check that the message is for me
         check_equal(request.header.receiver, self.aggregator.uuid, self.logger)
 
         # check that the message is for my federation
-        check_equal(
-            request.header.federation_uuid, self.aggregator.federation_uuid, self.logger)
+        check_equal(request.header.federation_uuid,
+                    self.aggregator.federation_uuid, self.logger)
 
         # check that we agree on the single cert common name
-        check_equal(
-            request.header.single_col_cert_common_name,
-            self.aggregator.single_col_cert_common_name,
-            self.logger
-        )
+        check_equal(request.header.single_col_cert_common_name,
+                    self.aggregator.single_col_cert_common_name, self.logger)
 
     def GetTasks(self, request, context):  # NOQA:N802
         """Request a job from aggregator.
@@ -151,7 +165,8 @@ class AggregatorGRPCServer(aggregator_pb2_grpc.AggregatorServicer):
         This method handles a request from a collaborator for a job.
 
         Args:
-            request (aggregator_pb2.GetTasksRequest): The request from the collaborator.
+            request (aggregator_pb2.GetTasksRequest): The request from the
+                collaborator.
             context (grpc.ServicerContext): The context of the request.
 
         Returns:
@@ -166,18 +181,15 @@ class AggregatorGRPCServer(aggregator_pb2_grpc.AggregatorServicer):
             if isinstance(tasks[0], str):
                 # backward compatibility
                 tasks_proto = [
-                    aggregator_pb2.Task(
-                        name=task,
-                    ) for task in tasks
+                    aggregator_pb2.Task(name=task, ) for task in tasks
                 ]
             else:
                 tasks_proto = [
-                    aggregator_pb2.Task(
-                        name=task.name,
-                        function_name=task.function_name,
-                        task_type=task.task_type,
-                        apply_local=task.apply_local
-                    ) for task in tasks
+                    aggregator_pb2.Task(name=task.name,
+                                        function_name=task.function_name,
+                                        task_type=task.task_type,
+                                        apply_local=task.apply_local)
+                    for task in tasks
                 ]
         else:
             tasks_proto = []
@@ -187,20 +199,22 @@ class AggregatorGRPCServer(aggregator_pb2_grpc.AggregatorServicer):
             round_number=round_number,
             tasks=tasks_proto,
             sleep_time=sleep_time,
-            quit=time_to_quit
-        )
+            quit=time_to_quit)
 
     def GetAggregatedTensor(self, request, context):  # NOQA:N802
         """Request a job from aggregator.
 
-        This method handles a request from a collaborator for an aggregated tensor.
+        This method handles a request from a collaborator for an aggregated
+        tensor.
 
         Args:
-            request (aggregator_pb2.GetAggregatedTensorRequest): The request from the collaborator.
+            request (aggregator_pb2.GetAggregatedTensorRequest): The request
+                from the collaborator.
             context (grpc.ServicerContext): The context of the request.
 
         Returns:
-            aggregator_pb2.GetAggregatedTensorResponse: The response to the request.
+            aggregator_pb2.GetAggregatedTensorResponse: The response to the
+                request.
         """
         self.validate_collaborator(request, context)
         self.check_request(request)
@@ -212,25 +226,28 @@ class AggregatorGRPCServer(aggregator_pb2_grpc.AggregatorServicer):
         tags = tuple(request.tags)
 
         named_tensor = self.aggregator.get_aggregated_tensor(
-            collaborator_name, tensor_name, round_number, report, tags, require_lossless)
+            collaborator_name, tensor_name, round_number, report, tags,
+            require_lossless)
 
         return aggregator_pb2.GetAggregatedTensorResponse(
             header=self.get_header(collaborator_name),
             round_number=round_number,
-            tensor=named_tensor
-        )
+            tensor=named_tensor)
 
     def SendLocalTaskResults(self, request, context):  # NOQA:N802
         """Request a model download from aggregator.
 
-        This method handles a request from a collaborator to send the results of a local task.
+        This method handles a request from a collaborator to send the results
+        of a local task.
 
         Args:
-            request (aggregator_pb2.SendLocalTaskResultsRequest): The request from the collaborator.
+            request (aggregator_pb2.SendLocalTaskResultsRequest): The request
+                from the collaborator.
             context (grpc.ServicerContext): The context of the request.
 
         Returns:
-            aggregator_pb2.SendLocalTaskResultsResponse: The response to the request.
+            aggregator_pb2.SendLocalTaskResultsResponse: The response to the
+                request.
         """
         try:
             proto = aggregator_pb2.TaskResults()
@@ -249,17 +266,18 @@ class AggregatorGRPCServer(aggregator_pb2_grpc.AggregatorServicer):
         round_number = proto.round_number
         data_size = proto.data_size
         named_tensors = proto.tensors
-        self.aggregator.send_local_task_results(
-            collaborator_name, round_number, task_name, data_size, named_tensors)
+        self.aggregator.send_local_task_results(collaborator_name,
+                                                round_number, task_name,
+                                                data_size, named_tensors)
         # turn data stream into local model update
         return aggregator_pb2.SendLocalTaskResultsResponse(
-            header=self.get_header(collaborator_name)
-        )
+            header=self.get_header(collaborator_name))
 
     def get_server(self):
         """Return gRPC server.
-        
-        This method creates a gRPC server if it does not already exist and returns it.
+
+        This method creates a gRPC server if it does not already exist and
+        returns it.
 
         Returns:
             grpc.Server: The gRPC server.
@@ -289,10 +307,9 @@ class AggregatorGRPCServer(aggregator_pb2_grpc.AggregatorServicer):
                 self.logger.warn('Client-side authentication is disabled.')
 
             self.server_credentials = ssl_server_credentials(
-                ((private_key_b, certificate_b),),
+                ((private_key_b, certificate_b), ),
                 root_certificates=root_certificate_b,
-                require_client_auth=not self.disable_client_auth
-            )
+                require_client_auth=not self.disable_client_auth)
 
             self.server.add_secure_port(self.uri, self.server_credentials)
 
@@ -300,8 +317,9 @@ class AggregatorGRPCServer(aggregator_pb2_grpc.AggregatorServicer):
 
     def serve(self):
         """Start an aggregator gRPC service.
-        
-        This method starts the gRPC server and handles requests until all quit jobs havebeen sent.
+
+        This method starts the gRPC server and handles requests until all quit
+        jobs havebeen sent.
         """
         self.get_server()
 
