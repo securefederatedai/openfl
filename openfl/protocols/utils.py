cc9aff66a544474ae0f540f109942379ad8c51a6
manuelhsantana <manuel.santana.castolo@intel.com>
2024-07-08 13:01:17 -0700

    Update protocols functions docstring for fix lint flake8 warnings
    Signed-off-by: manuelhsantana <manuel.santana.castolo@intel.com>

--- openfl/protocols/utils.py
+++ openfl/protocols/utils.py
@@ -13,10 +13,10 @@ def model_proto_to_bytes_and_metadata(model_proto):
         model_proto: The protobuf of the model.
 
     Returns:
-        bytes_dict: A dictionary where the keys are tensor names and the values are 
-            the corresponding tensor data in bytes.
-        metadata_dict: A dictionary where the keys are tensor names and the values are 
-            the corresponding tensor metadata.
+        bytes_dict: A dictionary where the keys are tensor names and the
+            values are the corresponding tensor data in bytes.
+        metadata_dict: A dictionary where the keys are tensor names and the
+            values are the corresponding tensor metadata.
         round_number: The round number for the model.
     """
     bytes_dict = {}
@@ -28,16 +28,13 @@ def model_proto_to_bytes_and_metadata(model_proto):
             'int_to_float': proto.int_to_float,
             'int_list': proto.int_list,
             'bool_list': proto.bool_list
-        }
-            for proto in tensor_proto.transformer_metadata
-        ]
+        } for proto in tensor_proto.transformer_metadata]
         if round_number is None:
             round_number = tensor_proto.round_number
         else:
             assert round_number == tensor_proto.round_number, (
                 f'Round numbers in model are inconsistent: {round_number} '
-                f'and {tensor_proto.round_number}'
-            )
+                f'and {tensor_proto.round_number}')
     return bytes_dict, metadata_dict, round_number
 
 
@@ -46,18 +43,20 @@ def bytes_and_metadata_to_model_proto(bytes_dict, model_id, model_version,
     """Convert bytes and metadata to model protobuf.
 
     Args:
-        bytes_dict: A dictionary where the keys are tensor names and the values are the 
-            corresponding tensor data in bytes.
+        bytes_dict: A dictionary where the keys are tensor names and the 
+            values are the corresponding tensor data in bytes.
         model_id: The ID of the model.
         model_version: The version of the model.
         is_delta: A flag indicating whether the model is a delta model.
-        metadata_dict: A dictionary where the keys are tensor names and the values are the 
-            corresponding tensor metadata.
+        metadata_dict: A dictionary where the keys are tensor names and the
+            values are the corresponding tensor metadata.
 
     Returns:
         model_proto: The protobuf of the model.
     """
-    model_header = ModelHeader(id=model_id, version=model_version, is_delta=is_delta)  # NOQA:F821
+    model_header = ModelHeader(id=model_id,
+                               version=model_version,
+                               is_delta=is_delta)  # NOQA:F821
 
     tensor_protos = []
     for key, data_bytes in bytes_dict.items():
@@ -78,18 +77,22 @@ def bytes_and_metadata_to_model_proto(bytes_dict, model_id, model_version,
                 bool_list = metadata.get('bool_list')
             else:
                 bool_list = []
-            metadata_protos.append(base_pb2.MetadataProto(
-                int_to_float=int_to_float,
-                int_list=int_list,
-                bool_list=bool_list,
-            ))
-        tensor_protos.append(TensorProto(name=key,  # NOQA:F821
-                                         data_bytes=data_bytes,
-                                         transformer_metadata=metadata_protos))
+            metadata_protos.append(
+                base_pb2.MetadataProto(
+                    int_to_float=int_to_float,
+                    int_list=int_list,
+                    bool_list=bool_list,
+                ))
+        tensor_protos.append(
+            TensorProto(
+                name=key,  # NOQA:F821
+                data_bytes=data_bytes,
+                transformer_metadata=metadata_protos))
     return base_pb2.ModelProto(header=model_header, tensors=tensor_protos)
 
 
-def construct_named_tensor(tensor_key, nparray, transformer_metadata, lossless):
+def construct_named_tensor(tensor_key, nparray, transformer_metadata,
+                           lossless):
     """Construct named tensor.
 
     Args:
@@ -117,11 +120,12 @@ def construct_named_tensor(tensor_key, nparray, transformer_metadata, lossless):
             bool_list = metadata.get('bool_list')
         else:
             bool_list = []
-        metadata_protos.append(base_pb2.MetadataProto(
-            int_to_float=int_to_float,
-            int_list=int_list,
-            bool_list=bool_list,
-        ))
+        metadata_protos.append(
+            base_pb2.MetadataProto(
+                int_to_float=int_to_float,
+                int_list=int_list,
+                bool_list=bool_list,
+            ))
 
     tensor_name, origin, round_number, report, tags = tensor_key
 
@@ -136,12 +140,13 @@ def construct_named_tensor(tensor_key, nparray, transformer_metadata, lossless):
     )
 
 
-def construct_proto(tensor_dict, model_id, model_version, is_delta, compression_pipeline):
+def construct_proto(tensor_dict, model_id, model_version, is_delta,
+                    compression_pipeline):
     """Construct proto.
 
     Args:
-        tensor_dict: A dictionary where the keys are tensor names and the values are the 
-            corresponding tensors.
+        tensor_dict: A dictionary where the keys are tensor names and the
+            values are the corresponding tensors.
         model_id: The ID of the model.
         model_version: The version of the model.
         is_delta: A flag indicating whether the model is a delta model.
@@ -155,14 +160,17 @@ def construct_proto(tensor_dict, model_id, model_version, is_delta, compression_
     bytes_dict = {}
     metadata_dict = {}
     for key, array in tensor_dict.items():
-        bytes_dict[key], metadata_dict[key] = compression_pipeline.forward(data=array)
+        bytes_dict[key], metadata_dict[key] = compression_pipeline.forward(
+            data=array)
 
-    # convert the compressed_tensor_dict and metadata to protobuf, and make the new model proto
-    model_proto = bytes_and_metadata_to_model_proto(bytes_dict=bytes_dict,
-                                                    model_id=model_id,
-                                                    model_version=model_version,
-                                                    is_delta=is_delta,
-                                                    metadata_dict=metadata_dict)
+    # convert the compressed_tensor_dict and metadata to protobuf, and make
+    # the new model proto
+    model_proto = bytes_and_metadata_to_model_proto(
+        bytes_dict=bytes_dict,
+        model_id=model_id,
+        model_version=model_version,
+        is_delta=is_delta,
+        metadata_dict=metadata_dict)
     return model_proto
 
 
@@ -170,8 +178,8 @@ def construct_model_proto(tensor_dict, round_number, tensor_pipe):
     """Construct model proto from tensor dict.
 
     Args:
-        tensor_dict: A dictionary where the keys are tensor names and the values are the 
-            corresponding tensors.
+        tensor_dict: A dictionary where the keys are tensor names and the
+            values are the corresponding tensors.
         round_number: The round number for the model.
         tensor_pipe: The tensor pipe for the model.
 
@@ -183,13 +191,14 @@ def construct_model_proto(tensor_dict, round_number, tensor_pipe):
     named_tensors = []
     for key, nparray in tensor_dict.items():
         bytes_data, transformer_metadata = tensor_pipe.forward(data=nparray)
-        tensor_key = TensorKey(key, 'agg', round_number, False, ('model',))
-        named_tensors.append(construct_named_tensor(
-            tensor_key,
-            bytes_data,
-            transformer_metadata,
-            lossless=True,
-        ))
+        tensor_key = TensorKey(key, 'agg', round_number, False, ('model', ))
+        named_tensors.append(
+            construct_named_tensor(
+                tensor_key,
+                bytes_data,
+                transformer_metadata,
+                lossless=True,
+            ))
 
     return base_pb2.ModelProto(tensors=named_tensors)
 
@@ -197,44 +206,46 @@ def construct_model_proto(tensor_dict, round_number, tensor_pipe):
 def deconstruct_model_proto(model_proto, compression_pipeline):
     """Deconstruct model proto.
 
-    This function takes a model protobuf and a compression pipeline, 
-    and deconstructs the protobuf into a dictionary of tensors and a round number.
+    This function takes a model protobuf and a compression pipeline,
+    and deconstructs the protobuf into a dictionary of tensors and a round
+    number.
 
     Args:
         model_proto: The protobuf of the model.
         compression_pipeline: The compression pipeline for the model.
 
     Returns:
-        tensor_dict: A dictionary where the keys are tensor names and the values are the 
-            corresponding tensors.
+        tensor_dict: A dictionary where the keys are tensor names and the
+        values are the corresponding tensors.
         round_number: The round number for the model.
     """
     # extract the tensor_dict and metadata
-    bytes_dict, metadata_dict, round_number = model_proto_to_bytes_and_metadata(model_proto)
+    bytes_dict, metadata_dict, round_number = model_proto_to_bytes_and_metadata(
+        model_proto)
 
     # decompress the tensors
     # TODO: Handle tensors meant to be held-out from the compression pipeline
     #  (currently none are held out).
     tensor_dict = {}
     for key in bytes_dict:
-        tensor_dict[key] = compression_pipeline.backward(data=bytes_dict[key],
-                                                         transformer_metadata=metadata_dict[key])
+        tensor_dict[key] = compression_pipeline.backward(
+            data=bytes_dict[key], transformer_metadata=metadata_dict[key])
     return tensor_dict, round_number
 
 
 def deconstruct_proto(model_proto, compression_pipeline):
     """Deconstruct the protobuf.
 
-    This function takes a model protobuf and a compression pipeline, and deconstructs the 
-    protobuf into a dictionary of tensors.
+    This function takes a model protobuf and a compression pipeline, and
+    deconstructs the protobuf into a dictionary of tensors.
 
     Args:
         model_proto: The protobuf of the model.
         compression_pipeline: The compression pipeline for the model.
 
     Returns:
-        tensor_dict: A dictionary where the keys are tensor names and the values are the 
-            corresponding tensors.
+        tensor_dict: A dictionary where the keys are tensor names and the
+            values are the corresponding tensors.
     """
     # extract the tensor_dict and metadata
     bytes_dict, metadata_dict = model_proto_to_bytes_and_metadata(model_proto)
@@ -244,8 +255,8 @@ def deconstruct_proto(model_proto, compression_pipeline):
     #  (currently none are held out).
     tensor_dict = {}
     for key in bytes_dict:
-        tensor_dict[key] = compression_pipeline.backward(data=bytes_dict[key],
-                                                         transformer_metadata=metadata_dict[key])
+        tensor_dict[key] = compression_pipeline.backward(
+            data=bytes_dict[key], transformer_metadata=metadata_dict[key])
     return tensor_dict
 
 
@@ -297,7 +308,8 @@ def datastream_to_proto(proto, stream, logger=None):
             logger.debug(f'datastream_to_proto parsed a {type(proto)}.')
         return proto
     else:
-        raise RuntimeError(f'Received empty stream message of type {type(proto)}')
+        raise RuntimeError(
+            f'Received empty stream message of type {type(proto)}')
 
 
 def proto_to_datastream(proto, logger, max_buffer_size=(2 * 1024 * 1024)):
@@ -306,7 +318,8 @@ def proto_to_datastream(proto, logger, max_buffer_size=(2 * 1024 * 1024)):
     Args:
         proto: The protobuf to be converted into a data stream.
         logger: The logger for logging information.
-        max_buffer_size (optional): The maximum buffer size for the data stream. Defaults to 2*1024*1024.
+        max_buffer_size (optional): The maximum buffer size for the data
+            stream. Defaults to 2*1024*1024.
 
     Yields:
         reply: Chunks of the data stream for the remote connection.
@@ -314,10 +327,12 @@ def proto_to_datastream(proto, logger, max_buffer_size=(2 * 1024 * 1024)):
     npbytes = proto.SerializeToString()
     data_size = len(npbytes)
     buffer_size = data_size if max_buffer_size > data_size else max_buffer_size
-    logger.debug(f'Setting stream chunks with size {buffer_size} for proto of type {type(proto)}')
+    logger.debug(
+        f'Setting stream chunks with size {buffer_size} for proto of type {type(proto)}'
+    )
 
     for i in range(0, data_size, buffer_size):
-        chunk = npbytes[i: i + buffer_size]
+        chunk = npbytes[i:i + buffer_size]
         reply = base_pb2.DataStream(npbytes=chunk, size=len(chunk))
         yield reply
 
@@ -329,7 +344,7 @@ def get_headers(context) -> dict:
         context: The context containing the headers.
 
     Returns:
-        headers: A dictionary where the keys are header names and the 
+        headers: A dictionary where the keys are header names and the
             values are the corresponding header values.
     """
     return {header[0]: header[1] for header in context.invocation_metadata()}
