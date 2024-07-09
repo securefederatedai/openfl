0e11ed62738f38ddb0ffee2215be5772284a2191
Shah, Karan <karan.shah@intel.com>
2024-07-09 04:21:37 +0000

    Point linter and formatter to full openfl namespace
    Signed-off-by: Shah, Karan <karan.shah@intel.com>

--- shell/lint.sh
+++ shell/lint.sh
@@ -3,9 +3,8 @@ set -Eeuo pipefail
 
 base_dir=$(dirname $(dirname $0))
 
-# TODO: @karansh1 Apply across all modules
-isort --sp "${base_dir}/pyproject.toml" --check openfl/experimental
+isort --sp "${base_dir}/pyproject.toml" --check openfl
 
-black --config "${base_dir}/pyproject.toml" --check openfl/experimental
+black --config "${base_dir}/pyproject.toml" --check openfl
 
-flake8 --config "${base_dir}/setup.cfg" openfl/experimental
+flake8 --config "${base_dir}/setup.cfg" openfl
