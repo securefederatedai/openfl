966e4428db928c95f7e6a810e6236262c6160dff
Ishant Thakare <ishantrog752@gmail.com>
2024-12-02 18:30:48 +0530

    Adding testcases for FederatedRuntime
    Signed-off-by: Ishant Thakare <ishantrog752@gmail.com>

---
+++ tests/github/experimental/workflow/FederatedRuntime/testcase_private_attributes_initialization_without_callable/director/private_attributes/aggregator_private_attrs.py
@@ -0,0 +1,6 @@
+# Copyright (C) 2020-2024 Intel Corporation
+# SPDX-License-Identifier: Apache-2.0
+
+import numpy as np
+
+aggregator_private_attributes = {"test_loader_agg": np.random.rand(10, 28, 28)}
