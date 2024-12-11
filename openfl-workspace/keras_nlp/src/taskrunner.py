58363a45b64500eb2faa52a93be77f07763820a3
yes <shailesh.tanwar@intel.com>
2024-12-11 00:28:39 -0800

    removed keras as ke
    Signed-off-by: yes <shailesh.tanwar@intel.com>

--- openfl-workspace/keras_nlp/src/taskrunner.py
+++ openfl-workspace/keras_nlp/src/taskrunner.py
@@ -4,7 +4,7 @@
 Licensed subject to the terms of the separately executed evaluation
 license agreement between Intel Corporation and you.
 """
-import keras as ke
+import keras
 
 from openfl.federated import KerasTensorFlowTaskRunner
 
@@ -42,27 +42,27 @@ class KerasNLP(KerasTensorFlowTaskRunner):
         Returns:
             tensorflow.python.keras.engine.sequential.Sequential: The model defined in Keras
         """
-        encoder_inputs = ke.Input(shape=(None, num_encoder_tokens))
-        encoder = ke.layers.LSTM(latent_dim, return_state=True)
+        encoder_inputs = keras.Input(shape=(None, num_encoder_tokens))
+        encoder = keras.layers.LSTM(latent_dim, return_state=True)
         encoder_outputs, state_h, state_c = encoder(encoder_inputs)
 
         # We discard `encoder_outputs` and only keep the states.
         encoder_states = [state_h, state_c]
 
         # Set up the decoder, using `encoder_states` as initial state.
-        decoder_inputs = ke.Input(shape=(None, num_decoder_tokens))
+        decoder_inputs = keras.Input(shape=(None, num_decoder_tokens))
 
         # We set up our decoder to return full output sequences,
         # and to return internal states as well. We don't use the
         # return states in the training model, but we will use them in inference.
-        decoder_lstm = ke.layers.LSTM(latent_dim, return_sequences=True, return_state=True)
+        decoder_lstm = keras.layers.LSTM(latent_dim, return_sequences=True, return_state=True)
         decoder_outputs, _, _ = decoder_lstm(decoder_inputs, initial_state=encoder_states)
-        decoder_dense = ke.layers.Dense(num_decoder_tokens, activation='softmax')
+        decoder_dense = keras.layers.Dense(num_decoder_tokens, activation='softmax')
         decoder_outputs = decoder_dense(decoder_outputs)
 
         # Define the model that will turn
         # `encoder_input_data` & `decoder_input_data` into `decoder_target_data`
-        model = ke.Model([encoder_inputs, decoder_inputs], decoder_outputs)
+        model = keras.Model([encoder_inputs, decoder_inputs], decoder_outputs)
 
         model.compile(
             optimizer="RMSprop",
