94f2bc7ae49d757c7d9e662b4786bca361126278
yes <shailesh.tanwar@intel.com>
2024-12-10 23:43:54 -0800

    formatting issue
    Signed-off-by: yes <shailesh.tanwar@intel.com>

--- openfl/federated/__init__.py
+++ openfl/federated/__init__.py
@@ -14,7 +14,11 @@ if util.find_spec("tensorflow") is not None:
     from openfl.federated.data import FederatedDataSet  # NOQA
     from openfl.federated.data import KerasDataLoader, TensorFlowDataLoader
     from openfl.federated.task import FederatedModel  # NOQA
-    from openfl.federated.task import KerasTaskRunner, TensorFlowTaskRunner, KerasTensorFlowTaskRunner
+    from openfl.federated.task import (
+        KerasTaskRunner,
+        TensorFlowTaskRunner,
+        KerasTensorFlowTaskRunner,
+    )
 if util.find_spec("torch") is not None:
     from openfl.federated.data import FederatedDataSet  # NOQA
     from openfl.federated.data import PyTorchDataLoader
