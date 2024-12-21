38f7c30c57aa5d92db1ecf0c35a22be6733ed30d
Shah, Karan <karan.shah@intel.com>
2024-12-21 18:00:46 +0530

    Remove all traces of log_metric_callback and write_metric
    Signed-off-by: Shah, Karan <karan.shah@intel.com>

--- openfl-workspace/torch_cnn_mnist_fed_eval/src/mnist_utils.py
+++ openfl-workspace/torch_cnn_mnist_fed_eval/src/mnist_utils.py
@@ -6,26 +6,10 @@
 from logging import getLogger
 
 import numpy as np
-from torch.utils.tensorboard import SummaryWriter
 from torchvision import datasets
 from torchvision import transforms
 
 logger = getLogger(__name__)
-
-writer = None
-
-
-def get_writer():
-    """Create global writer object."""
-    global writer
-    if not writer:
-        writer = SummaryWriter('./logs/cnn_mnist', flush_secs=5)
-
-
-def write_metric(node_name, task_name, metric_name, metric, round_number):
-    """Write metric callback."""
-    get_writer()
-    writer.add_scalar(f'{node_name}/{task_name}/{metric_name}', metric, round_number)
 
 
 def one_hot(labels, classes):
--- openfl/utilities/logs.py
+++ openfl/utilities/logs.py
@@ -8,36 +8,6 @@ import logging
 
 from rich.console import Console
 from rich.logging import RichHandler
-from tensorboardX import SummaryWriter
-
-writer = None
-
-
-def get_writer():
-    """Create global writer object.
-
-    This function creates a global `SummaryWriter` object for logging to
-    TensorBoard.
-    """
-    global writer
-    if not writer:
-        writer = SummaryWriter("./logs/tensorboard", flush_secs=5)
-
-
-def write_metric(node_name, task_name, metric_name, metric, round_number):
-    """Write metric callback.
-
-    This function logs a metric to TensorBoard.
-
-    Args:
-        node_name (str): The name of the node.
-        task_name (str): The name of the task.
-        metric_name (str): The name of the metric.
-        metric (float): The value of the metric.
-        round_number (int): The current round number.
-    """
-    get_writer()
-    writer.add_scalar(f"{node_name}/{task_name}/{metric_name}", metric, round_number)
 
 
 def setup_loggers(log_level=logging.INFO):
