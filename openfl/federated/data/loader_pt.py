5a2b16385dafedc30807e83547ba3cde51bf8fd5
yes <shailesh.tanwar@intel.com>
2024-12-22 21:21:19 -0800

    monkey patch added
    Signed-off-by: yes <shailesh.tanwar@intel.com>

--- openfl/federated/data/loader_pt.py
+++ openfl/federated/data/loader_pt.py
@@ -4,11 +4,15 @@
 
 """PyTorchDataLoader module."""
 
+import sys
 from math import ceil
 
 import numpy as np
+import typing_extensions
 
 from openfl.federated.data.loader import DataLoader
+
+sys.modules["pip._vendor.typing_extensions"] = typing_extensions
 
 
 class PyTorchDataLoader(DataLoader):
