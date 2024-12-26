894888131db35b6c94cf683ffeada9461de23dcb
Chaurasiya, Payal <payal.chaurasiya@intel.com>
2024-12-26 07:29:01 -0800

    Fix memory logs and create pdf
    Signed-off-by: Chaurasiya, Payal <payal.chaurasiya@intel.com>

--- tests/end_to_end/test_suites/memory_logs_tests.py
+++ tests/end_to_end/test_suites/memory_logs_tests.py
@@ -9,6 +9,7 @@ import json
 from tests.end_to_end.utils.common_fixtures import fx_federation_tr, fx_federation_tr_dws
 import tests.end_to_end.utils.constants as constants
 from tests.end_to_end.utils import federation_helper as fed_helper, ssh_helper as ssh
+from tests.end_to_end.utils.generate_report import generate_memory_report
 
 log = logging.getLogger(__name__)
 
@@ -79,6 +80,8 @@ def _log_memory_usage(request, fed_obj):
 
     # Log the aggregator memory usage details
     memory_usage_dict = _convert_to_json(aggregator_memory_usage_file)
+    aggregator_path = os.path.join(fed_obj.workspace_path, "aggregator")
+    generate_memory_report(memory_usage_dict, aggregator_path)
 
     # check memory usage entries for each round
     assert (
@@ -99,6 +102,8 @@ def _log_memory_usage(request, fed_obj):
         ), f"Memory usage file for collaborator {collaborator.collaborator_name} is not available"
 
         memory_usage_dict = _convert_to_json(collaborator_memory_usage_file)
+        collaborator_path = os.path.join(fed_obj.workspace_path, collaborator.name)
+        generate_memory_report(memory_usage_dict, collaborator_path)
 
         assert (
             len(memory_usage_dict) == request.config.num_rounds
