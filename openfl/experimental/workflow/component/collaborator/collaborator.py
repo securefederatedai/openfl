589692d49da4b4e822d100e133cf26893ff08f52
Pant, Akshay <akshay.akshaypant@gmail.com>
2025-01-02 13:24:16 +0530

    restructure: linting change
    Signed-off-by: Pant, Akshay <akshay.akshaypant@gmail.com>

--- openfl/experimental/workflow/component/collaborator/collaborator.py
+++ openfl/experimental/workflow/component/collaborator/collaborator.py
@@ -164,10 +164,7 @@ class Collaborator:
             f"Round {self.round_number}," f" collaborator {self.name} is sending results..."
         )
         self.client.send_task_results(
-            self.name,
-            self.round_number,
-            next_step,
-            cloudpickle.dumps(clone)
+            self.name, self.round_number, next_step, cloudpickle.dumps(clone)
         )
 
     def get_tasks(self) -> Tuple:
