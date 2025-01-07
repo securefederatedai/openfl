a460f4127e34c311437b3e53c0426c236f3be0f0
yes <shailesh.tanwar@intel.com>
2025-01-06 22:12:37 -0800

    code changes
    Signed-off-by: yes <shailesh.tanwar@intel.com>

--- tests/github/test_hello_federation.py
+++ tests/github/test_hello_federation.py
@@ -20,9 +20,9 @@ def main():
     excluded_dirs = ['workspace', 'default', "experimental"]
     for root, _, files in os.walk('openfl-workspace'):
         if any(file.endswith(".workspace") for file in files):
-            for dir in str(root).split(str('openfl-workspace') + "\\"):
+            for dir in str(root).split(str('openfl-workspace') + os.path.sep):
                 if dir and not any(dir.startswith(prefix) for prefix in excluded_dirs):
-                    workspace_choice.append(dir.replace("openfl-workspace/", "").replace("\\", "/"))
+                    workspace_choice.append(dir)
     parser.add_argument('--template', default='keras/cnn_mnist', choices=workspace_choice)
     parser.add_argument('--fed_workspace', default='fed_work12345alpha81671')
     parser.add_argument('--col1', default='one123dragons')
