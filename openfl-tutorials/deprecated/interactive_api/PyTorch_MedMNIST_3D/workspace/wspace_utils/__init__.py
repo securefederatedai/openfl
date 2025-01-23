f1235b1dc0115d78564cce5c6798a402159436e3
MasterSkepticista <karan.shah@intel.com>
2025-01-23 13:00:40 +0530

    Remove notebooks
    Signed-off-by: MasterSkepticista <karan.shah@intel.com>

--- openfl-tutorials/deprecated/interactive_api/PyTorch_MedMNIST_3D/workspace/wspace_utils/__init__.py
+++
@@ -1,7 +0,0 @@
-# Copyright (C) 2020-2021 Intel Corporation
-# SPDX-License-Identifier: Apache-2.0
-
-from .utils import Transform3D
-from .utils import model_to_syncbn
-
-__all__ = ["Transform3D", "model_to_syncbn"]
