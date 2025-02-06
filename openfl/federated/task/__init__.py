739a6155842a60b21ad6a4960a5dc8c95fc438db
yes <shailesh.tanwar@intel.com>
2025-02-06 05:43:37 -0800

    code changes
    Signed-off-by: yes <shailesh.tanwar@intel.com>

--- openfl/federated/task/__init__.py
+++ openfl/federated/task/__init__.py
@@ -10,12 +10,6 @@ from importlib import util
 from openfl.federated.task.runner import TaskRunner  # NOQA
 
 if util.find_spec("keras") is not None:
-    if util.find_spec("torch") is not None:
-        os.environ["KERAS_BACKEND"] = "torch"
-    elif util.find_spec("jax") is not None:
-        os.environ["KERAS_BACKEND"] = "jax"
-    elif util.find_spec("tensorflow") is not None:
-        os.environ["KERAS_BACKEND"] = "tensorflow"
     from openfl.federated.task.fl_model import FederatedModel  # NOQA
     from openfl.federated.task.runner_keras import KerasTaskRunner  # NOQA
 if util.find_spec("torch") is not None:
