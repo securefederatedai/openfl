b587e5d8b87b04357fbb0d35cd9329aa1e3ee12a
noopur <noopur@intel.com>
2025-02-06 15:30:58 +0000

    Assert increase in current round after restart + wait
    Signed-off-by: noopur <noopur@intel.com>

--- tests/end_to_end/test_suites/tr_resiliency_tests.py
+++ tests/end_to_end/test_suites/tr_resiliency_tests.py
@@ -40,7 +40,7 @@ def test_federation_via_native_with_restarts(request, fx_federation_tr):
 
     time.sleep(20)
     round_post_agg_restart = fed_helper.get_current_round(fx_federation_tr.aggregator.tensor_db_path)
-    assert round_post_agg_restart >= current_round, "Current round is not updated after aggregator restart"
+    assert round_post_agg_restart > current_round, f"Expected current round to be ahead of {current_round} after aggregator restart"
 
     # Restart collaborators
     assert int_helper.restart_participants(fx_federation_tr.collaborators)
@@ -48,7 +48,7 @@ def test_federation_via_native_with_restarts(request, fx_federation_tr):
 
     time.sleep(20)
     round_post_collab_restart = fed_helper.get_current_round(fx_federation_tr.aggregator.tensor_db_path)
-    assert round_post_collab_restart >= round_post_agg_restart, "Current round is not updated after collaborators restart"
+    assert round_post_collab_restart > round_post_agg_restart, f"Expected current round to be ahead of {round_post_agg_restart} after collaborators restart"
 
     # Restart all participants
     assert int_helper.restart_participants(fx_federation_tr.collaborators+[fx_federation_tr.aggregator])
@@ -56,7 +56,7 @@ def test_federation_via_native_with_restarts(request, fx_federation_tr):
 
     time.sleep(20)
     round_post_all_restart = fed_helper.get_current_round(fx_federation_tr.aggregator.tensor_db_path)
-    assert round_post_all_restart >= round_post_collab_restart, "Current round is not updated after all participants restart"
+    assert round_post_all_restart > round_post_collab_restart, f"Expected current round to be ahead of {round_post_collab_restart} after all participants restart"
 
     log.info("Current round number is increasing after every restart as expected.")
 
@@ -95,7 +95,7 @@ def test_federation_via_dws_with_restarts(request, fx_federation_tr_dws):
 
     time.sleep(20)
     round_post_agg_restart = fed_helper.get_current_round(fx_federation_tr_dws.aggregator.tensor_db_path)
-    assert round_post_agg_restart >= current_round, "Current round is not updated after aggregator restart"
+    assert round_post_agg_restart > current_round, f"Expected current round to be ahead of {current_round} after aggregator restart"
 
     # Restart collaborators
     assert int_helper.restart_participants(fx_federation_tr_dws.collaborators)
@@ -103,7 +103,7 @@ def test_federation_via_dws_with_restarts(request, fx_federation_tr_dws):
 
     time.sleep(20)
     round_post_collab_restart = fed_helper.get_current_round(fx_federation_tr_dws.aggregator.tensor_db_path)
-    assert round_post_collab_restart >= round_post_agg_restart, "Current round is not updated after collaborators restart"
+    assert round_post_collab_restart > round_post_agg_restart, f"Expected current round to be ahead of {round_post_agg_restart} after collaborators restart"
 
     # Restart all participants
     assert int_helper.restart_participants(fx_federation_tr_dws.collaborators+[fx_federation_tr_dws.aggregator])
@@ -111,7 +111,7 @@ def test_federation_via_dws_with_restarts(request, fx_federation_tr_dws):
 
     time.sleep(20)
     round_post_all_restart = fed_helper.get_current_round(fx_federation_tr_dws.aggregator.tensor_db_path)
-    assert round_post_all_restart >= round_post_collab_restart, "Current round is not updated after all participants restart"
+    assert round_post_all_restart > round_post_collab_restart, f"Expected current round to be ahead of {round_post_collab_restart} after all participants restart"
 
     log.info("Current round number is increasing after every restart as expected.")
 
