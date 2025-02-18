c3f38445bfee0dd8941189fcd634c25bbb670811
Shailesh Pant <shailesh.pant@intel.com>
2025-02-18 05:15:24 +0000

    Enable rest protocol between Aggregator and Collaborator - add new AggregatorClientInterface to allow switching b/w grpc and rest - endhance existing AggregatorGRPCClient to start using AggregatorClientInterface - added new transport package for rest with AggregatorRESTClient implementing AggregatorClientInterface - added streaming api support with custom content-type - added various connection flag for streaming request - send additional header key "Sender" for better request logging at server side - aligned Rest and gRPC client for most of the init params - added AggregatorRESTServer and necesary changes in aggregator cli and federated/plan get_server method - added transport_protocol settings in defaults/network.yaml, defaulted the same to 'grpc' - reduced cyclomatic complexity of Rest Server - fixed protobuf streaming issue for v1/task/results API - added more detailed logging for task progression and metadata for each api calls - pinned Flask version to latest stable 3.1.0
    - addressing review comments - 13th-May
    - added ping api and `collaborato` constructor hint for `AggregatorClientInterface`
    - added send_message_to_server in client and AggregatorClientInterface, Rest Server is already at parity
    - changed base uri for REST server to 'experimental/v1', adjusted the client and tests accordingly
    - fixed issue related to mTLS in REST server/client
    - disabled TLS 1.2 in both server/client
    rebased 21st.May.1
    
    Signed-off-by: Shailesh Pant <shailesh.pant@intel.com>

---
+++ openfl/transport/rest/__init__.py
@@ -0,0 +1,6 @@
+# Copyright 2020-2025 Intel Corporation
+# SPDX-License-Identifier: Apache-2.0
+
+
+from openfl.transport.rest.aggregator_client import AggregatorRESTClient
+from openfl.transport.rest.aggregator_server import AggregatorRESTServer
