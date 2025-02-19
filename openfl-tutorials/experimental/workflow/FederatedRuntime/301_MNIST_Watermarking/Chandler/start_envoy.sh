68b34d244a1f75cb2ea81d1718298990fcc9f43e
Teodor Parvanov <teodor.parvanov@intel.com>
2025-02-19 11:22:19 +0100

    Fixing the CLI commands for starting the envoys
    Signed-off-by: Teodor Parvanov <teodor.parvanov@intel.com>

--- openfl-tutorials/experimental/workflow/FederatedRuntime/301_MNIST_Watermarking/Chandler/start_envoy.sh
+++ openfl-tutorials/experimental/workflow/FederatedRuntime/301_MNIST_Watermarking/Chandler/start_envoy.sh
@@ -3,4 +3,4 @@ set -e
 ENVOY_NAME=$1
 ENVOY_CONF=$2
 
-fx envoy start -n "$ENVOY_NAME" --disable-tls -c "$ENVOY_CONF"
+fx envoy start -n "$ENVOY_NAME" --disable-tls -ec "$ENVOY_CONF" -dh localhost -dp 50050
