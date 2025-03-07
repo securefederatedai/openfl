51a09d1ba19186dfbf43450cd6310dbd8d71327c
yes <shailesh.tanwar@intel.com>
2025-03-06 23:42:39 -0800

    code changes
    Signed-off-by: yes <shailesh.tanwar@intel.com>

--- tests/github/utils.py
+++ tests/github/utils.py
@@ -51,6 +51,8 @@ def create_certified_workspace(path, template, fqdn, rounds_to_train):
     check_call(['fx', 'workspace', 'create', '--prefix', path, '--template', template])
     os.chdir(path)
     check_call(['pip', 'install', '-r', 'requirements.txt'])
+    # Check installed packages
+    check_call(['pip', 'freeze'])
 
     # Initialize FL plan
     check_call(['fx', 'plan', 'initialize', '-a', fqdn])
