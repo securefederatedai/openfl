0172973a593691f166e1098006ee55286fdbcbfa
Pant, Akshay <akshay.akshaypant@gmail.com>
2025-03-20 18:15:06 +0530

    fix(plan): change verify to non-static method
    Signed-off-by: Pant, Akshay <akshay.akshaypant@gmail.com>

--- openfl/federated/plan/plan.py
+++ openfl/federated/plan/plan.py
@@ -137,7 +137,7 @@ class Plan:
             plan.authorized_cols = Plan.load(cols_config_path).get("collaborators", [])
 
             Plan._load_collaborator_data_paths(plan, data_config_path)
-            Plan.verify(plan)
+            plan.verify()
 
             if resolve:
                 plan.resolve()
@@ -679,21 +679,17 @@ class Plan:
             logger.error(f"Failed to create or save model proto: {e}")
             raise
 
-    @staticmethod
-    def verify(plan):
+    def verify(self):
         """
         This function checks for inconsistencies in the plan config, for example, checks if two
         non-compatible features are enabled at the same time.
-
-        Args:
-            plan: Federated Learning plan object that needs to be verified.
         """
-        if plan.config["aggregator"][SETTINGS].get("secure_aggregation"):
+        if self.config["aggregator"][SETTINGS].get("secure_aggregation"):
             # TODO: Secure aggregation requires all collaborators to participate in all rounds and
             # can not tolerate dropouts. Hence, if `secure_aggregation: true` in aggregator, there
             # should be no `straggler_handling_policy` defined or use `WaitForAllPolicy` (default
             # straggler handling policy).
-            straggler_handling_policy = plan.config.get(
+            straggler_handling_policy = self.config.get(
                 "straggler_handling_policy",
                 {
                     TEMPLATE: "openfl.component.aggregator.straggler_handling.WaitForAllPolicy",
