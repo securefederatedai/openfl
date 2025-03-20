35afe2ad838e0620de6e2f3841e839b06ebeac0e
Pant, Akshay <akshay.akshaypant@gmail.com>
2025-03-20 16:52:01 +0530

    feat(plan): add plan verify to check for incompatible features
    Signed-off-by: Pant, Akshay <akshay.akshaypant@gmail.com>

--- openfl/federated/plan/plan.py
+++ openfl/federated/plan/plan.py
@@ -137,6 +137,7 @@ class Plan:
             plan.authorized_cols = Plan.load(cols_config_path).get("collaborators", [])
 
             Plan._load_collaborator_data_paths(plan, data_config_path)
+            Plan.verify(plan)
 
             if resolve:
                 plan.resolve()
@@ -677,3 +678,33 @@ class Plan:
         except Exception as e:
             logger.error(f"Failed to create or save model proto: {e}")
             raise
+
+    @staticmethod
+    def verify(plan):
+        """
+        This function checks for inconsistencies in the plan config, for example, checks if two
+        non-compatible features are enabled at the same time.
+
+        Args:
+            plan: Federated Learning plan object that needs to be verified.
+        """
+        if plan.config["aggregator"][SETTINGS].get("secure_aggregation"):
+            # TODO: Secure aggregation requires all collaborators to participate in all rounds and
+            # can not tolerate dropouts. Hence, if `secure_aggregation: true` in aggregator, there
+            # should be no `straggler_handling_policy` defined or use `WaitForAllPolicy` (default
+            # straggler handling policy).
+            straggler_handling_policy = plan.config.get(
+                "straggler_handling_policy",
+                {
+                    TEMPLATE: "openfl.component.aggregator.straggler_handling.WaitForAllPolicy",
+                    SETTINGS: {},
+                },
+            )
+            if straggler_handling_policy.get(TEMPLATE) not in [
+                "openfl.component.WaitForAllPolicy",
+                "openfl.component.aggregator.WaitForAllPolicy",
+                "openfl.component.aggregator.straggler_handling.WaitForAllPolicy",
+            ]:
+                raise Exception(
+                    "Only WaitForAllPolicy straggler handling is supported with secure aggregation."
+                )
