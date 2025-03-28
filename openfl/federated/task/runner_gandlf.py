9d3d6795c425ebf4a30b3dc4ec92ad07bae17cce
sarthakpati <sarthak.pati@hotmail.com>
2025-03-28 11:31:43 -0400

    revert
    Signed-off-by: sarthakpati <sarthak.pati@hotmail.com>

--- openfl/federated/task/runner_gandlf.py
+++ openfl/federated/task/runner_gandlf.py
@@ -75,11 +75,12 @@ class GaNDLFTaskRunner(TaskRunner):
         if isinstance(gandlf_config, str) and os.path.exists(gandlf_config):
             gandlf_config = yaml.safe_load(open(gandlf_config, "r"))
 
-        try:
-            gandlf_config = ConfigManager(gandlf_config)
-        except Exception:
-            self.logger.info("WARNING: GANDLF.config_manager.ConfigManager did not work as expected.")
+        gandlf_config = ConfigManager(gandlf_config)
+
+        (
+            model,
             optimizer,
+            train_loader,
             val_loader,
             scheduler,
             params,
