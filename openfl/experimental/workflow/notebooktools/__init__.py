38a5f7bcf4cf68c5335b8415b9c09924056eee99
refai06 <149057514+refai06@users.noreply.github.com>
2025-04-07 18:48:52 +0530

    [Workflow Interface] Rename and Refactor WorkspaceExport into NotebookTools (#1364)
    * Experimental-export-module-refactoring
    
    Signed-off-by: refai06 <refai.ahamed06@gmail.com>
    
    * Incorporated comments
    
    Signed-off-by: refai06 <refai.ahamed06@gmail.com>
    
    * Improvements added
    
    Signed-off-by: refai06 <refai.ahamed06@gmail.com>
    
    * Added testcase
    
    Signed-off-by: refai06 <refai.ahamed06@gmail.com>
    
    * Code Enhancement
    
    Signed-off-by: refai06 <refai.ahamed06@gmail.com>
    
    * Docstring & loggers update
    
    Signed-off-by: refai06 <refai.ahamed06@gmail.com>
    
    * Added NotebookTools module Testcase
    
    Signed-off-by: refai06 <refai.ahamed06@gmail.com>
    
    * Testcase update
    
    Signed-off-by: refai06 <refai.ahamed06@gmail.com>
    
    * Review_comments_incorporated
    
    Signed-off-by: refai06 <refai.ahamed06@gmail.com>
    
    * Fix testcase
    
    Signed-off-by: refai06 <refai.ahamed06@gmail.com>
    
    * Added review comments and testcase update
    
    Signed-off-by: refai06 <refai.ahamed06@gmail.com>
    
    * Review comments
    
    Signed-off-by: refai06 <refai.ahamed06@gmail.com>
    
    * Remove testcase
    
    Signed-off-by: refai06 <refai.ahamed06@gmail.com>
    
    * Enhance code and incorporate review comments
    
    Signed-off-by: refai06 <refai.ahamed06@gmail.com>
    
    * Code changes
    
    Signed-off-by: refai06 <refai.ahamed06@gmail.com>
    
    * revert tutorial change
    
    Signed-off-by: refai06 <refai.ahamed06@gmail.com>
    
    * Incorporate comments
    
    Signed-off-by: refai06 <refai.ahamed06@gmail.com>
    
    * Docstring update
    
    Signed-off-by: refai06 <refai.ahamed06@gmail.com>
    
    ---------
    
    Signed-off-by: refai06 <refai.ahamed06@gmail.com>
    Signed-off-by: yes <shailesh.tanwar@intel.com>

---
+++ openfl/experimental/workflow/notebooktools/__init__.py
@@ -0,0 +1,8 @@
+# Copyright 2020-2025 Intel Corporation
+# SPDX-License-Identifier: Apache-2.0
+
+
+"""openfl.experimental.workflow.notebooktools package."""
+
+from openfl.experimental.workflow.notebooktools.code_analyzer import CodeAnalyzer
+from openfl.experimental.workflow.notebooktools.notebook_tools import NotebookTools
