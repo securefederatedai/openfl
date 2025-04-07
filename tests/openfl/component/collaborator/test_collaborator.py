76d8134eb2a390fd5d2a64cad89542dee08193b4
Pant, Akshay <akshay.akshaypant@gmail.com>
2025-04-07 09:35:22 +0530

    fix(tests): collaborator tensor codec changes
    Signed-off-by: Pant, Akshay <akshay.akshaypant@gmail.com>

--- tests/openfl/component/collaborator/test_collaborator.py
+++ tests/openfl/component/collaborator/test_collaborator.py
@@ -8,9 +8,10 @@ import numpy
 import pytest
 
 from openfl.component.collaborator import Collaborator
+from openfl.pipelines import NoCompressionPipeline
 from openfl.protocols import base_pb2
+from openfl.transport.serialiser import CollaboratorSerialiser
 from openfl.utilities.types import TensorKey
-
 
 @pytest.fixture
 def collaborator_mock():
@@ -18,6 +19,9 @@ def collaborator_mock():
     col = Collaborator('col1', 'some_uuid', 'federation_uuid',
                        mock.Mock(), mock.Mock(), mock.Mock(), opt_treatment='RESET')
     col.tensor_db = mock.Mock()
+    col._serialisation_middleware = CollaboratorSerialiser(
+        col.collaborator_name, mock.Mock(), NoCompressionPipeline()
+    )
 
     return col
 
@@ -93,18 +97,18 @@ def test_do_task(collaborator_mock, tensor_key):
     collaborator_mock.send_task_results.assert_called_with(result[0], round_number, task.name)
 
 
+# FIXME
 def test_send_task_results(collaborator_mock, tensor_key):
     """Test that send_task_results works correctly."""
     task_name = 'task_name'
-    tensor_key = tensor_key._replace(report=True)
+    tensor_key = tensor_key._replace(report=False)
     tensor_dict = {tensor_key: 0}
     round_number = 0
     data_size = -1
-    collaborator_mock.nparray_to_named_tensor = mock.Mock(return_value=None)
-    collaborator_mock.client.send_local_task_results = mock.Mock()
+    collaborator_mock._serialisation_middleware._aggregator_client.send_local_task_results = mock.Mock()
     collaborator_mock.send_task_results(tensor_dict, round_number, task_name)
 
-    collaborator_mock.client.send_local_task_results.assert_called_with(
+    collaborator_mock._serialisation_middleware._aggregator_client.send_local_task_results.assert_called_with(
         round_number, task_name, data_size, [None])
 
 
@@ -114,12 +118,11 @@ def test_send_task_results_train(collaborator_mock):
     tensor_dict = {}
     round_number = 0
     data_size = 200
-    collaborator_mock.nparray_to_named_tensor = mock.Mock()
     collaborator_mock.task_runner.get_train_data_size = mock.Mock(return_value=data_size)
-    collaborator_mock.client.send_local_task_results = mock.Mock()
+    collaborator_mock._serialisation_middleware._aggregator_client.send_local_task_results = mock.Mock()
     collaborator_mock.send_task_results(tensor_dict, round_number, task_name)
 
-    collaborator_mock.client.send_local_task_results.assert_called_with(
+    collaborator_mock._serialisation_middleware._aggregator_client.send_local_task_results.assert_called_with(
         round_number, task_name, data_size, [])
 
 
@@ -129,61 +132,23 @@ def test_send_task_results_valid(collaborator_mock):
     tensor_dict = {}
     round_number = 0
     data_size = 400
-    collaborator_mock.nparray_to_named_tensor = mock.Mock()
     collaborator_mock.task_runner.get_valid_data_size = mock.Mock(return_value=data_size)
-    collaborator_mock.client.send_local_task_results = mock.Mock()
+    collaborator_mock._serialisation_middleware._aggregator_client.send_local_task_results = mock.Mock()
     collaborator_mock.send_task_results(tensor_dict, round_number, task_name)
 
-    collaborator_mock.client.send_local_task_results.assert_called_with(
+    collaborator_mock._serialisation_middleware._aggregator_client.send_local_task_results.assert_called_with(
         round_number, task_name, data_size, [])
 
-
-def test_named_tensor_to_nparray_without_tags(collaborator_mock, named_tensor):
-    """Test that named_tensor_to_nparray works correctly for tensor without tags."""
-    nparray = collaborator_mock.named_tensor_to_nparray(named_tensor)
-
-    assert named_tensor.data_bytes == nparray
-
-
-@pytest.mark.parametrize('tag', ['compressed', 'lossy_compressed'])
-def test_named_tensor_to_nparray_compressed_tag(collaborator_mock, named_tensor, tag):
-    """Test that named_tensor_to_nparray works correctly for tensor with tags."""
-    named_tensor.tags.append(tag)
-    nparray = collaborator_mock.named_tensor_to_nparray(named_tensor)
-
-    assert isinstance(nparray, numpy.ndarray)
-
-
-def test_nparray_to_named_tensor(collaborator_mock, tensor_key, named_tensor):
-    """Test that nparray_to_named_tensor works correctly."""
-    named_tensor.tags.append('compressed')
-    nparray = collaborator_mock.named_tensor_to_nparray(named_tensor)
-    tensor = collaborator_mock.nparray_to_named_tensor(tensor_key, nparray)
-    assert tensor.data_bytes == named_tensor.data_bytes
-    assert tensor.lossless is True
-
-
-def test_nparray_to_named_tensor_trained(collaborator_mock, tensor_key_trained, named_tensor):
-    """Test that nparray_to_named_tensor works correctly for trained tensor."""
-    named_tensor.tags.append('compressed')
-    collaborator_mock.use_delta_updates = True
-    nparray = collaborator_mock.named_tensor_to_nparray(named_tensor)
-    collaborator_mock.tensor_db.get_tensor_from_cache = mock.Mock(
-        return_value=nparray)
-    tensor = collaborator_mock.nparray_to_named_tensor(tensor_key_trained, nparray)
-    assert len(tensor.data_bytes) == 32
-    assert tensor.lossless is False
-    assert 'delta' in tensor.tags
 
 
 @pytest.mark.parametrize('require_lossless', [True, False])
 def test_get_aggregated_tensor_from_aggregator(collaborator_mock, tensor_key,
                                                named_tensor, require_lossless):
     """Test that get_aggregated_tensor works correctly."""
-    collaborator_mock.client.get_aggregated_tensor = mock.Mock(return_value=named_tensor)
+    collaborator_mock._serialisation_middleware._aggregator_client.get_aggregated_tensor = mock.Mock(return_value=named_tensor)
     nparray = collaborator_mock.get_aggregated_tensor_from_aggregator(tensor_key, require_lossless)
 
-    collaborator_mock.client.get_aggregated_tensor.assert_called_with(
+    collaborator_mock._serialisation_middleware._aggregator_client.get_aggregated_tensor.assert_called_with(
         tensor_key.tensor_name, tensor_key.round_number,
         tensor_key.report, tensor_key.tags, require_lossless)
     assert nparray == named_tensor.data_bytes
@@ -225,8 +190,71 @@ def test_get_data_for_tensorkey_dependencies(collaborator_mock, tensor_key):
     tensor_key = tensor_key._replace(round_number=1)
     collaborator_mock.tensor_db.get_tensor_from_cache = mock.Mock(
         return_value=None)
-    collaborator_mock.tensor_codec.find_dependencies = mock.Mock(return_value=[tensor_key])
+    collaborator_mock._find_dependencies = mock.Mock(return_value=[tensor_key])
     collaborator_mock.get_aggregated_tensor_from_aggregator = mock.Mock()
     collaborator_mock.get_data_for_tensorkey(tensor_key)
     collaborator_mock.get_aggregated_tensor_from_aggregator.assert_called_with(
         tensor_key, require_lossless=True)
+
+def test_find_dependencies_without_send_model_deltas(collaborator_mock, tensor_key):
+    """Test that find_dependencies returns empty list when send_model_deltas = False."""
+    tensor_name, origin, _, report, _ = tensor_key
+    tensor_key = TensorKey(
+        tensor_name, origin, 5, report, ('model',)
+    )
+    tensor_key_dependencies = collaborator_mock._find_dependencies(tensor_key)
+
+    assert len(tensor_key_dependencies) == 0
+
+
+def test_find_dependencies_without_model_in_tags(collaborator_mock, tensor_key):
+    """Test that find_dependencies returns empty list when there is no model tag."""
+    tensor_key_dependencies = collaborator_mock._find_dependencies(tensor_key)
+
+    assert len(tensor_key_dependencies) == 0
+
+
+def test_find_dependencies_with_zero_round(collaborator_mock, tensor_key):
+    """Test that find_dependencies returns empty list when round number is 0."""
+    tensor_name, origin, round_number, report, tags = tensor_key
+    tensor_key = TensorKey(
+        tensor_name, origin, round_number, report, ('model',)
+    )
+    tensor_key_dependencies = collaborator_mock._find_dependencies(tensor_key)
+
+    assert len(tensor_key_dependencies) == 0
+
+
+def test_find_dependencies(collaborator_mock, tensor_key):
+    """Test that find_dependencies works correctly."""
+    collaborator_mock.use_delta_updates = True
+    tensor_name, origin, round_number, report, tags = tensor_key
+    round_number = 2
+    tensor_key = TensorKey(
+        tensor_name, origin, round_number, report, ('model',)
+    )
+    tensor_key_dependencies = collaborator_mock._find_dependencies(tensor_key)
+
+    assert len(tensor_key_dependencies) == 2
+    tensor_key_dependency_0, tensor_key_dependency_1 = tensor_key_dependencies
+    assert tensor_key_dependency_0.round_number == round_number - 1
+    assert tensor_key_dependency_0.tags == tensor_key.tags
+    assert tensor_key_dependency_1.tags == ('aggregated', 'delta', 'compressed')
+
+
+def test_find_dependencies_is_lossy(collaborator_mock, tensor_key):
+    """Test that find_dependencies works correctly with lossy_compressed."""
+    collaborator_mock.use_delta_updates = True
+    collaborator_mock.compression_pipeline.is_lossy = mock.Mock(return_value=True)
+    tensor_name, origin, round_number, report, tags = tensor_key
+    round_number = 2
+    tensor_key = TensorKey(
+        tensor_name, origin, round_number, report, ('model',)
+    )
+    tensor_key_dependencies = collaborator_mock._find_dependencies(tensor_key)
+
+    assert len(tensor_key_dependencies) == 2
+    tensor_key_dependency_0, tensor_key_dependency_1 = tensor_key_dependencies
+    assert tensor_key_dependency_0.round_number == round_number - 1
+    assert tensor_key_dependency_0.tags == tensor_key.tags
+    assert tensor_key_dependency_1.tags == ('aggregated', 'delta', 'lossy_compressed')
