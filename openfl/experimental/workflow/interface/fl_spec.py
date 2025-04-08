ee20a50dbdea7f88b60d405811375b286ba5b3d2
Sachin Gupta <sachin.gupta.dsp@gmail.com>
2025-04-08 21:27:12 +0530

    Initial Commit: Moved checkpoint for next (FLSpec) to execute_agg_steps, execute_collab_steps (LocalRuntime). Enhanced filtering
    Signed-off-by: Sachin Gupta <sachin.gupta.dsp@gmail.com>

--- openfl/experimental/workflow/interface/fl_spec.py
+++ openfl/experimental/workflow/interface/fl_spec.py
@@ -334,10 +334,6 @@ class FLSpec:
         parent = inspect.stack()[1][3]
         parent_func = getattr(self, parent)
 
-        if str(self._runtime) == "LocalRuntime":
-            # Checkpoint current attributes (if checkpoint==True)
-            checkpoint(self, parent_func)
-
         # Take back-up of current state of self
         agg_to_collab_ss = None
         if aggregator_to_collaborator(f, parent_func):
