81899d65a46f90b508b6d8564f84ca4293bc433f
Shah, Karan <karan.shah@intel.com>
2025-04-23 16:20:48 +0530

    Update docs
    Signed-off-by: Shah, Karan <karan.shah@intel.com>

--- openfl/callbacks/metric_writer.py
+++ openfl/callbacks/metric_writer.py
@@ -15,9 +15,11 @@ logger = logging.getLogger(__name__)
 class MetricWriter(Callback):
     """Log scalar metrics at the end of each round.
 
+    This callback logs scalar metrics to a file and optionally to TensorBoard.
+    To enable TensorBoard logging, set the environment variable `TENSORBOARD=1`.
+
     Attributes:
         log_dir: Path to write logs as lines of JSON. Defaults to `./logs`.
-        use_tensorboard: If True, writes scalar summaries to TensorBoard under `log_dir`.
     """
 
     def __init__(self, log_dir: str = "./logs/"):
