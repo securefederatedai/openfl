8ca8d46ea4dd8a116f708cc707b19eab40a80f11
Shah, Karan <kbshah1998@outlook.com>
2025-04-30 17:22:06 +0530

    Task callback rebase
    Signed-off-by: Shah, Karan <kbshah1998@outlook.com>

--- openfl/component/collaborator/collaborator.py
+++ openfl/component/collaborator/collaborator.py
@@ -221,6 +221,7 @@ class Collaborator:
             array = self.get_data_for_tensorkey(tensor_key)
             input_tensor_dict.update({tensor_key.tensor_name: array})
 
+        self.callbacks.on_task_begin(round_number)
         # now we have whatever the model needs to do the task
         # Tasks are defined as methods of TaskRunner
         func = getattr(self.task_runner, func_name)
@@ -232,6 +233,9 @@ class Collaborator:
             input_tensor_dict=input_tensor_dict,
             **kwargs,
         )
+
+        self.callbacks.on_task_end(round_number)
+
         # If secure aggregation is enabled, add masks to the dict to be shared
         # with the aggregator.
         if self._secure_aggregation_enabled:
