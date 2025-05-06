5e95d73efbbade53c75cd9a857d99eb0ee9b46d8
Sachin Gupta <sachin.gupta.dsp@gmail.com>
2025-05-06 12:57:52 +0530

    Removed changes in runtime_utils.py
    Signed-off-by: Sachin Gupta <sachin.gupta.dsp@gmail.com>

--- openfl/experimental/workflow/utilities/runtime_utils.py
+++ openfl/experimental/workflow/utilities/runtime_utils.py
@@ -45,16 +45,14 @@ def parse_attrs(ctx, exclude=[], reserved_words=["next", "runtime", "input"]):
     return cls_attrs, valid_artifacts
 
 
-def generate_artifacts(
-    ctx, reserved_words=["next", "runtime", "input", "checkpoint", "execute_task_args"]
-):
+def generate_artifacts(ctx, reserved_words=["next", "runtime", "input", "checkpoint"]):
     """Generates artifacts from the given context, excluding specified reserved
     words.
 
     Args:
         ctx (any): The context to generate artifacts from.
         reserved_words (list, optional): A list of reserved words to exclude.
-            Defaults to ["next", "runtime", "input", "checkpoint", "execute_task_args"].
+            Defaults to ["next", "runtime", "input", "checkpoint"].
 
     Returns:
         tuple: A tuple containing a generator of artifacts and a list of
@@ -154,14 +152,14 @@ def _process_exclusion(ctx, cls_attrs, exclude_list, f):
             delattr(ctx, attr)
 
 
-def checkpoint(ctx, parent_func, chkpnt_reserved_words=["next", "runtime", "execute_task_args"]):
+def checkpoint(ctx, parent_func, chkpnt_reserved_words=["next", "runtime"]):
     """Optionally saves the current state for the task just executed.
 
     Args:
         ctx (any): The context to checkpoint.
         parent_func (function): The function that was just executed.
         chkpnt_reserved_words (list, optional): A list of reserved words to
-            exclude from checkpointing. Defaults to ["next", "runtime", "execute_task_args"].
+            exclude from checkpointing. Defaults to ["next", "runtime"].
 
     Returns:
         step_stdout (io.StringIO): parent_func stdout
