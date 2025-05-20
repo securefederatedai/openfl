32be41c6b3386d85e2054a75b29c0c15393e89cd
payalcha <payal.chaurasiya@intel.com>
2025-05-20 08:47:05 +0000

    report
    Signed-off-by: payalcha <payal.chaurasiya@intel.com>

--- .github/scripts/coverage-report.sh
+++ .github/scripts/coverage-report.sh
@@ -22,21 +22,20 @@ rm -rf .coverage
 
 python -m pytest -rA --cov=openfl 
 
-python -m pytest -s tests/end_to_end/test_suites/task_runner_tests.py -k test_federation_via_native --model_name keras/mnist --num_rounds 2 --disable_client_auth --secure_agg --cov-report=term-missing --cov-append --cov=openfl  
+python -m pytest -s tests/end_to_end/test_suites/task_runner_tests.py -k test_federation_via_native --model_name torch/mnist --num_rounds 2 --disable_client_auth --secure_agg --cov-report=term-missing --cov-append --cov=openfl  
 
 python -m pytest -s tests/end_to_end/test_suites/task_runner_tests.py -k test_federation_via_native --model_name keras/jax/mnist --num_rounds 2 --disable_tls --cov-report=term-missing --cov-append --cov=openfl 
 
-python -m pytest -s tests/end_to_end/test_suites/memory_logs_tests.py -k test_log_memory_usage_basic --model_name keras/mnist --num_rounds 2 --log_memory_usage --secure_agg --cov-report=term-missing --cov-append --cov=openfl  
+python -m pytest -s tests/end_to_end/test_suites/memory_logs_tests.py -k test_log_memory_usage_basic --model_name torch/histology --num_rounds 2 --log_memory_usage --secure_agg --cov-report=term-missing --cov-append --cov=openfl  
 
-python -m pytest -s tests/end_to_end/test_suites/tr_resiliency_tests.py --model_name torch/mnist --num_rounds 25 --cov-report=term-missing --cov-append --cov=openfl 
+python -m pytest -s tests/end_to_end/test_suites/tr_resiliency_tests.py --model_name keras/torch/mnist --num_rounds 25 --cov-report=term-missing --cov-append --cov=openfl 
 
 python -m pytest -s tests/end_to_end/test_suites/tr_flower_tests.py -k test_flower_app_pytorch_native --model_name flower-app-pytorch --num_rounds 1 --cov-report=term-missing --cov-append --cov=openfl 
 
-coverage report
 
 python -m pytest -s tests/end_to_end/test_suites/task_runner_tests.py -m task_runner_dockerized_ws --num_rounds 2 --model_name keras/torch/mnist --cov-report=term-missing --cov-append --cov=openfl 
 
-python -m pytest -s tests/end_to_end/test_suites/tr_with_fedeval_tests.py -m task_runner_basic --model_name keras/mnist --num_rounds 1 --cov-report=term-missing --cov-append --cov=openfl 
+python -m pytest -s tests/end_to_end/test_suites/tr_with_fedeval_tests.py -m task_runner_basic --model_name xgb_higgs  --num_rounds 2 --cov-report=term-missing --cov-append --cov=openfl 
 
 python -m pytest -s tests/end_to_end/test_suites/wf_local_func_tests.py --num_rounds 2 --cov-report=term-missing --cov-append --cov=openfl 
 
@@ -48,7 +47,7 @@ python -m pytest -s tests/end_to_end/test_suites/wf_federated_runtime_tests.py -
 
 python -m pytest -s tests/end_to_end/test_suites/wf_federated_runtime_tests.py -k test_federated_runtime_secure_aggregation --cov-report=term-missing  --cov-append --cov=openfl 
 
-python -m pytest -s tests/end_to_end/test_suites/wf_federated_runtime_tests.py -k test_federated_evaluation --cov-report=term-missing  --cov-append --cov=openfl 
+# python -m pytest -s tests/end_to_end/test_suites/wf_federated_runtime_tests.py -k test_federated_evaluation --cov-report=term-missing  --cov-append --cov=openfl 
 
 fx experimental deactivate 
 # Combine and generate the final coverage report
